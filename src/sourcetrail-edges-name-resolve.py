6f5ea314ec31c7b3ca83887dec1ba3cd30a7a0ee
Vitaly R <mortiv16@gmail.com>
2020-01-15 01:21:07 +0300

    added notebook for merging with codesearchnet

---
+++ src/sourcetrail-edges-name-resolve.py
@@ -0,0 +1,20 @@
+import pandas as p
+import sys
+
+nodes = p.read_csv(sys.argv[1])
+
+edges = p.read_csv(sys.argv[2])
+
+from pprint import pprint
+df_dict = nodes[['id', 'serialized_name']].to_dict()
+ids = df_dict['id'].values()
+serialized_name = df_dict['serialized_name'].values()
+id_map = dict(zip(ids, serialized_name))
+
+def map_name(node_id):
+    return id_map[node_id]
+
+edges['source_node_id'] = edges['source_node_id'].apply(map_name)
+edges['target_node_id'] = edges['target_node_id'].apply(map_name)
+
+edges.to_csv('normalized_sourcetrail_edges.csv', index=False)
