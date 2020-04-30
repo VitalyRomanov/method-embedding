8c23c2e8072675af0bf175c1076aff8a7bd326f1
Vitaly R <mortiv16@gmail.com>
2020-05-01 00:12:53 +0300

    update

--- graph-network/classifiers.py
+++ graph-network/classifiers.py
@@ -12,7 +12,7 @@ class LRClassifier(Model):
                             kernel_regularizer=regularizers.l2(0.001))
                             # input_shape=(input_size,))
 
-    def __call__(self, x, training=False):
+    def __call__(self, x, training=False, **kwargs):
         return self.logits(x)
 
 
@@ -20,14 +20,14 @@ class NNClassifier(Model):
     def __init__(self, input_size=None):
         super(NNClassifier, self).__init__()
         self.l1 = Dense(50,
-                            kernel_regularizer=regularizers.l2(0.001),
+                            # kernel_regularizer=regularizers.l2(0.001),
                             input_shape=(input_size,))
         self.l2 = Dense(30, input_shape=(50,))
         self.l3 = Dense(10, input_shape=(30,))
         self.logits = Dense(2, input_shape=(10,))
 
 
-    def __call__(self, x, training=False, **kwargs):
+    def __call__(self, x, **kwargs):
         x = self.l1(x)
         x = self.l2(x)
         x = self.l3(x)
