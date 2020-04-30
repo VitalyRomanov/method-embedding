fb0a524200a90d7c271f58d7501328852f5d7d0e
Vitaly R <mortiv16@gmail.com>
2020-05-01 00:13:53 +0300

    experiments for improving speed

--- graph-network/Embedder.py
+++ graph-network/Embedder.py
@@ -7,10 +7,14 @@ class Embedder:
         aid, iid = zip(*id_map.items())
         self.inv = dict(zip(iid, aid))
 
+
     def __getitem__(self, key):
+        # if not hasattr(self, "map_id"):
+        #     self.map_id = np.vectorize(lambda id: self.ind[id])
         if type(key) == int:
             return self.e[self.ind[key], :]
         elif type(key) == np.ndarray:
+            # return self.e[self.map_id(key), :]
             return self.e[np.array([self.ind[k] for k in key], dtype=np.int32), :]
         else:
             raise TypeError("Unknown type:", type(key))
