e38e2d0ebdfc053dd49d98240a2e44f4dbe4af5b
Vitaly R <mortiv16@gmail.com>
2020-05-01 00:14:27 +0300

    improved interfaces

--- graph-network/run_experiment.py
+++ graph-network/run_experiment.py
@@ -1,18 +1,47 @@
-from Experiments import Experiments, Experiment
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
 
-BASE_PATH = "/home/ltv/data/local_run/graph-network/GAT-2020-03-24-03-37-36-421131"
+from Experiments import Experiments, Experiment
+import argparse
+
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('experiment', default=None,
+                    help='Select experiment [apicall|link|typeuse|varuse|fname|nodetype]')
+args = parser.parse_args()
+
+"""
+apicall creates an experiment where we try to predict the exstense of "next call" link between nodes. 
+    Currently, the process is trained using negative sampling procedure and additional classifier. Negative sampling 
+    is done uniformly form the collection of all the rest of the nodes. The number of negative samples is the batch is 
+    equal to the number of positive samples. The embeddigns themselves are not trained, only weights of classifier are 
+    updated. 
+"""
+
+# TODO
+# can maybe the scores I'm getting are overfit?
+# Need to generate not random edges, but negative edges for the same destination. There can be a situation where
+# I'm getting good results simply because negative edges do not touch positive nodes
+
+# BASE_PATH = "/home/ltv/data/local_run/graph-network/GAT-2020-03-24-03-37-36-421131" # trained on node types
+# BASE_PATH = "/home/ltv/dev/method-embedding/graph-network/models/GAT-2020-04-24-00-21-11-377353" # trained on function names
+# BASE_PATH = "/home/ltv/dev/method-embedding/graph-network/models/GAT-2020-04-23-22-33-41-890539" # trained on function names with classifier
+BASE_PATH = "/home/ltv/dev/method-embedding/graph-network/models/GAT-2020-04-24-19-07-55-139367" # trained on next call
+# BASE_PATH = "/home/ltv/dev/method-embedding/graph-network/models/GAT-2020-04-24-22-12-19-718445" # trained on variables
 # API_SEARCH = "/Volumes/External/datasets/Code/source-graphs/python-source-graph/04_api_sequence_calls/flat_calls.csv"
 API_SEQ = "/home/ltv/data/datasets/source_code/python-source-graph/04_api_sequence_calls/flat_calls.csv"
+VAR_USE = "/home/ltv/data/datasets/source_code/python-source-graph/03_variables_on_functions/node_to_var.csv"
 
 e = Experiments(base_path=BASE_PATH,
                 api_seq_path=API_SEQ,
-                type_use_path=None,
-                node_type_path=None,
-                variable_use_path=None,
+                type_use_path=None, #not needed
+                node_type_path=None, #not needed
+                variable_use_path=VAR_USE, #not needed
                 function_name_path=None
                 )
 
-EXPERIMENT_NAME = "fname"
+# EXPERIMENT_NAME = "fname"
+EXPERIMENT_NAME = args.experiment
 
 experiment = e[EXPERIMENT_NAME]
 
@@ -41,7 +70,7 @@ from classifiers import LRClassifier, NNClassifier, ElementPredictor, NodeClassi
 import tensorflow as tf
 
 if EXPERIMENT_NAME in {'link', 'apicall', 'typeuse'}:
-    clf = LRClassifier(experiment.embed_size)
+    clf = NNClassifier(experiment.embed_size)
 elif EXPERIMENT_NAME in {'varuse', 'fname'}:
     clf = ElementPredictor(experiment.embed_size, experiment.unique_elements, 100)
 elif EXPERIMENT_NAME in {'nodetype'}:
@@ -51,7 +80,7 @@ elif EXPERIMENT_NAME in {'nodetype'}:
 #             loss='sparse_categorical_crossentropy')
 
 loss_object = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-optimizer = tf.keras.optimizers.Adam(learning_rate=0.01)
+optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
 
 train_loss = tf.keras.metrics.Mean(name='train_loss')
 train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='train_accuracy')
@@ -59,7 +88,7 @@ train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='train_accuracy
 test_loss = tf.keras.metrics.Mean(name='test_loss')
 test_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='test_accuracy')
 
-# @tf.function
+@tf.function
 def train_step(batch):
     with tf.GradientTape() as tape:
         # training=True is only needed if there are layers with different
@@ -73,7 +102,7 @@ def train_step(batch):
     train_loss(loss)
     train_accuracy(batch["y"], predictions)
 
-# @tf.function
+@tf.function
 def test_step(batch):
     # training=False is only needed if there are layers with different
     # behavior during training versus inference (e.g. Dropout).
@@ -85,6 +114,8 @@ def test_step(batch):
 
 EPOCHS = 500
 
+print(f"\n\n\nExperiment name: {EXPERIMENT_NAME}")
+
 for epoch in range(EPOCHS):
     # Reset the metrics at the start of the next epoch
     train_loss.reset_states()
@@ -93,15 +124,17 @@ for epoch in range(EPOCHS):
     test_accuracy.reset_states()
 
 
-    for batch in experiment.train_batches():
+    for batch_ind, batch in enumerate(experiment.train_batches()):
         train_step(batch)
 
-        for batch in experiment.test_batches():
-            test_step(batch)
+        if batch_ind % 500 == 0:
 
-        template = 'Epoch {}, Loss: {:.4f}, Accuracy: {:.4f}, Test Loss: {:.4f}, Test Accuracy: {:.4f}'
-        print(template.format(epoch+1,
-                              train_loss.result(),
-                              train_accuracy.result()*100,
-                              test_loss.result(),
-                              test_accuracy.result()*100))
+            for batch in experiment.test_batches():
+                test_step(batch)
+
+            template = 'Epoch {}, Loss: {:.4f}, Accuracy: {:.4f}, Test Loss: {:.4f}, Test Accuracy: {:.4f}'
+            print(template.format(epoch+1,
+                                  train_loss.result(),
+                                  train_accuracy.result()*100,
+                                  test_loss.result(),
+                                  test_accuracy.result()*100))
