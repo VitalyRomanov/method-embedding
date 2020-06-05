a9a2c642ab02fdd64b26905004d2cf6500de6e50
Vitaly R <mortiv16@gmail.com>
2020-06-05 12:58:43 +0300

    automating indexing process

--- src/large_scale/install_envs.sh
+++ src/large_scale/install_envs.sh
@@ -2,18 +2,40 @@ conda activate python37
 
 while read repo
 do
-  cr_env=$(python -m venv $repo)
-  if [ -z "$cr_env" ]
+#  echo $repo/bin/python
+  # Create environment if does not exist
+  if [ ! -f "$repo/bin/python" ]
   then
-    echo "Created environment"
-    source $repo/bin/activate
-    act_env=""
-    if [ -z "$act_env" ]
+    echo "Creating environment for $repo"
+    if [ -z "$(python -m venv $repo)" ]
     then
-      echo "Activated environment"
-      echo "$(which pip)"
+      echo "Created environment for $repo"
+      cr_env=true
+    else
+      cr_env=false
+    fi
+  else
+    echo "Environment for $repo exists"
+    cr_env=true
+  fi
+
+  # Activate environment if created
+  if $cr_env; then
+    source $repo/bin/activate && act_env=true
+    echo "Activated $repo"
+  fi
+
+  # Process
+  if $act_env; then
+    # install packages
+    if [ ! -f "$repo/packages.txt" ]; then
+      echo "Installing packages for $repo"
       pip install $repo > $repo/piplog.log
       pip freeze > $repo/packages.txt
+    fi
+
+    if [ ! -f "$repo/$repo.srctrlprj" ]; then
+      echo "Creating Sourcetrail project for $repo"
       repo_lower=$(echo "$repo" | awk '{print tolower($0)}')
       echo "<?xml version="1.0" encoding="utf-8" ?>
 <config>
@@ -33,14 +55,78 @@ do
     </source_groups>
     <version>8</version>
 </config>" > $repo/$repo.srctrlprj
-      Sourcetrail.sh index -f $repo/$repo.srctrlprj > $repo/sourcetrail.log
-      deactivate
-    else
-      echo "Problems activating, skipping"
     fi
-  else
-    echo "Problems creating, skipping"
+
+    if [ ! -f "$repo/sourcetrail.log" ]; then
+      run_indexing=true
+    else
+      find_edges=$(cat "$repo/sourcetrail.log" | grep " Edges")
+      if [ -z "$find_edges" ]; then
+        echo "Indexing was interrupted, recovering..."
+        run_indexing=true
+      else
+        run_indexing=false
+      fi
+    fi
+
+    if $run_indexing; then
+      echo "Begin indexing"
+      Sourcetrail.sh index -i $repo/$repo.srctrlprj >> $repo/sourcetrail.log
+    else
+      echo "Already indexed"
+    fi
   fi
+
+  # Deactivate environment if activated
+  if $act_env; then
+    deactivate
+    echo "Deactivated $repo"
+  fi
+
+#  # Activate environment
+#  if [ -z "$cr_env" ]
+#  then
+#    source $repo/bin/activate
+#    act_env=""
+#
+#    if [ -z "$act_env" ]
+#    then
+#      echo "Activated environment"
+#      echo "$(which pip)"
+#
+#      if [[ -f "$repo/sourcetrail.log" ]]; then
+#
+#          echo "$FILE exist"
+#      fi
+#      pip install $repo > $repo/piplog.log
+#      pip freeze > $repo/packages.txt
+#      repo_lower=$(echo "$repo" | awk '{print tolower($0)}')
+#      echo "<?xml version="1.0" encoding="utf-8" ?>
+#<config>
+#    <source_groups>
+#        <source_group_c140f8c4-74c6-42e2-bc72-982c3f658806>
+#            <name>Python Source Group</name>
+#            <python_environment_path>.</python_environment_path>
+#            <source_extensions>
+#                <source_extension>.py</source_extension>
+#            </source_extensions>
+#            <source_paths>
+#                <source_path>lib/python3.7/site-packages/$repo_lower</source_path>
+#            </source_paths>
+#            <status>enabled</status>
+#            <type>Python Source Group</type>
+#        </source_group_c140f8c4-74c6-42e2-bc72-982c3f658806>
+#    </source_groups>
+#    <version>8</version>
+#</config>" > $repo/$repo.srctrlprj
+#      Sourcetrail.sh index -f $repo/$repo.srctrlprj > $repo/sourcetrail.log
+#      deactivate
+#    else
+#      echo "Problems activating, skipping"
+#    fi
+#  else
+#    echo "Problems creating, skipping"
+#  fi
 
 done < "${1:-/dev/stdin}"
 
