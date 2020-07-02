2b20fcb27fe02c2a7bea3a3328d7bb1343a29645
Vitaly R <mortiv16@gmail.com>
2020-07-02 12:34:25 +0300

    filter names with single occurrence

--- src/code_processing/extract_variable_names.py
+++ src/code_processing/extract_variable_names.py
@@ -1,13 +1,5 @@
-#%%
-import os
-import ast
-import sys
-import pandas
-# import javalang
-import javac_parser
-import ast
-# import plyj.parser as plyj
-
+import os, sys, ast, pandas, javac_parser
+from collections import Counter
 
 lang = sys.argv[1]
 working_directory = sys.argv[2]
@@ -17,21 +9,13 @@ if lang == "py":
 elif lang == "java":
     pass
     java = javac_parser.Java()
-    # parser = plyj.Parser()
 else:
     raise ValueError("Valid languages: py, java")
-
-# bodies_path = "/Volumes/External/datasets/Code/source-graphs/python-source-graph/function_bodies/functions.csv" # sys.argv[1]
-# nodes_path = "/Volumes/External/datasets/Code/source-graphs/python-source-graph/normalized_sourcetrail_nodes.csv" # sys.argv[2]
 
 bodies_path = os.path.join(working_directory, "source-graph-bodies.csv")
 nodes_path = os.path.join(working_directory, "normalized_sourcetrail_nodes.csv")
 
-#%%
-
 id_offset = pandas.read_csv(nodes_path)["id"].max() + 1
-
-# %%
 
 bodies = pandas.read_csv(bodies_path)[['id', 'body']]
 bodies.dropna(axis=0, inplace=True)
@@ -58,31 +42,9 @@ for body_ind, (ind, row) in enumerate(bodies.iterrows()):
             lines = row['body'].strip() #.split("\n")
             tokens = java.lex(lines)
             variables = [name for type, name, _, _, _ in tokens if type == "IDENTIFIER" and name not in not_local]
-            # for line in lines:
-            #     try:
-            #         # tree = parser.parse_expression(line)
-            #         tokens = javalang.tokenizer.tokenize(line.strip())
-            #         parser = javalang.parser.Parser(tokens)
-            #         tree = parser.parse_expression()
-            #         print(tree)
-            #         print(line)
-            #         # variables.extend([node.member for _, node in tree if type(node) is javalang.tree.MemberReference])
-            #     except:
-            #         pass
         else: continue
     except SyntaxError: # thrown by ast
         continue
-    # except javalang.parser.JavaSyntaxError: # thrown by javalang
-    #     continue
-    except TypeError: # thrown by javalang
-        pass
-    except StopIteration: # thrown by javalang
-        pass
-
-    # print(variables)
-    # print(row['body'].strip())
-    #
-    # import sys; sys.exit()
 
     for v in set(variables):
         if v not in variable_names:
@@ -105,9 +67,13 @@ print(" " * 30, end ="\r")
 
 #%%
 
+counter = Counter(map(lambda x: x[1], func_var_pairs))
+
 with open(os.path.join(working_directory, "source-graph-function-variable-pairs.csv"), "w") as fvpairs:
     fvpairs.write("src,dst\n")
     for func, var in func_var_pairs:
-        fvpairs.write(f"{func},{var}\n")
+        if counter[var] > 1:
+            fvpairs.write(f"{func},{var}\n")
 
-# %%
+# TODO:
+# string names should be embedded with char ngrams
