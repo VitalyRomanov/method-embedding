a60a809990d252adfba3d68320a5c170f723b0b2
Vitaly R <mortiv16@gmail.com>
2020-07-13 11:40:18 +0300

    update

--- src/large_scale/check_pip_exist.sh
+++ src/large_scale/check_pip_exist.sh
@@ -1,10 +1,15 @@
-for repo in $(awk -F"\t" '{print $2}')
+conda activate python37
+
+#for repo in $(awk -F"\t" '{print $2}')
+for repo in $(awk -F"\t" '{print $0}')
 do
-  var=$(conda search "$repo" | grep "^$repo .*py37")
-#  var=$(pip search "$repo" | grep "^$repo ")
+#  var=$(conda search "$repo" | grep "^$repo .*py37")
+  var=$(pip search "$repo" | grep "^$repo ")
 #  echo $var
   if [ -n "$var" ];
   then
     echo $repo
   fi
 done < "${1:-/dev/stdin}"
+
+conda deactivate
