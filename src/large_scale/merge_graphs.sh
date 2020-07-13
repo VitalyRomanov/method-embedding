9b9c24c47edbb9b7308f80b41e92678e387821e2
Vitaly R <mortiv16@gmail.com>
2020-07-13 09:03:09 +0300

    update

--- src/large_scale/merge_graphs.sh
+++ src/large_scale/merge_graphs.sh
@@ -16,14 +16,14 @@ for dir in $(ls $ENVS_DIR); do
     cd $ENVS_DIR/$dir
     echo "Found package $dir"
     if [ -f $dir.srctrldb ]; then
-#      sqlite3 $dir.srctrldb < $SQL_Q
+      sqlite3 $dir.srctrldb < $SQL_Q
       cd $RUN_DIR
-#      python verify_files.py $ENVS_DIR/$dir
-#      python ../sourcetrail/sourcetrail-node-name-merge.py $ENVS_DIR/$dir/nodes.csv
-#      python ../code_processing/call_seq_extractor.py $ENVS_DIR/$dir
-#      python ../code_processing/extract_variable_names.py py $ENVS_DIR/$dir
-#      python parse_bodies.py $ENVS_DIR/$dir
-#      python get_ast_edges.py $ENVS_DIR/$dir
+      python verify_files.py $ENVS_DIR/$dir
+      python ../sourcetrail/sourcetrail-node-name-merge.py $ENVS_DIR/$dir/nodes.csv
+      python ../code_processing/call_seq_extractor.py $ENVS_DIR/$dir
+      python ../code_processing/extract_variable_names.py py $ENVS_DIR/$dir
+      python parse_bodies.py $ENVS_DIR/$dir
+      python get_ast_edges.py $ENVS_DIR/$dir
     else
       echo "Package not indexed"
     fi
