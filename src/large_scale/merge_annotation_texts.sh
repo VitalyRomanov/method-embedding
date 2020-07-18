54eaed105a000e13e65e04279084b05301457955
Vitaly R <mortiv16@gmail.com>
2020-07-18 18:22:26 +0300

    fix bugs

--- src/large_scale/merge_annotation_texts.sh
+++ src/large_scale/merge_annotation_texts.sh
@@ -1,4 +1,4 @@
-conda activate python37
+conda activate scidoc
 
 ENVS_DIR=$1
 
@@ -7,7 +7,7 @@ for dir in $(ls $ENVS_DIR); do
   if [ -d $ENVS_DIR/$dir ]; then
     echo "Process $dir"
     if [ -f $ENVS_DIR/$dir/normalized_sourcetrail_nodes.csv ]; then
-      python $ENVS_DIR/$dir/source-graph-bodies_global.csv $ENVS_DIR/functions_with_annotations.jsonl
+      python extract_type_annotations.py $ENVS_DIR/$dir/source-graph-bodies.csv $ENVS_DIR/functions_with_annotations.jsonl
     fi
   fi
 done
