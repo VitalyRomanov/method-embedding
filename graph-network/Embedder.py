4f698cfa39482b7a43800549af02afa596df246e
Vitaly R <mortiv16@gmail.com>
2020-08-01 15:16:15 +0300

    added support for tf embedder

--- graph-network/Embedder.py
+++ graph-network/Embedder.py
@@ -1,4 +1,5 @@
 import numpy as np
+import tensorflow as tf
 
 class Embedder:
     def __init__(self, id_map, embeddings):
@@ -15,6 +16,13 @@ class Embedder:
             return self.e[self.ind[key], :]
         elif type(key) == np.ndarray:
             # return self.e[self.map_id(key), :]
-            return self.e[np.array([self.ind[k] for k in key], dtype=np.int32), :]
+            if isinstance(self.e, np.ndarray):
+                return self.e[np.array([self.ind[k] for k in key], dtype=np.int32), :]
+            # elif isinstance(self.e, tf.ResourceVariable):
+            else: # this is assumed to be tensorflow Variable, neet to work out how to do type checking
+                slices = np.array([self.ind[k] for k in key], dtype=np.int32)
+                return tf.gather(self.e, slices, axis=0)
+            # else:
+            #     raise TypeError("Problem with embedder internal type:", type(self.e))
         else:
             raise TypeError("Unknown type:", type(key))
