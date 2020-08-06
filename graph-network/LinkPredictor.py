619108222fa1f01636455aca8e257b0ba6ec7dce
Vitaly R <mortiv16@gmail.com>
2020-08-06 18:13:42 +0300

    change lr

--- graph-network/LinkPredictor.py
+++ graph-network/LinkPredictor.py
@@ -7,7 +7,7 @@ class LinkPredictor(nn.Module):
     def __init__(self, input_dimensionality):
         super(LinkPredictor, self).__init__()
 
-        self.norm = nn.BatchNorm1d(input_dimensionality)
+        self.norm = nn.BatchNorm1d(input_dimensionality) # LayerNorm
 
         self.l1 = nn.Linear(input_dimensionality, 20)
         self.l1_a = nn.Sigmoid()
@@ -15,6 +15,6 @@ class LinkPredictor(nn.Module):
         self.logits = nn.Linear(20, 2)
 
     def forward(self, x, **kwargs):
-        x = self.norm(x)
+        # x = self.norm(x)
         x = F.relu(self.l1(x))
         return self.logits(x)
