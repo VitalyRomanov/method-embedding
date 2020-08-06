8c49ecbc0027c5ec619c823000f5921e94a307d9
Vitaly R <mortiv16@gmail.com>
2020-08-06 10:34:09 +0300

    change lr

--- graph-network/train_multitask.py
+++ graph-network/train_multitask.py
@@ -276,7 +276,7 @@ def train(model, ee_fname, ee_varuse, ee_apicall, lp_fname, lp_varuse, lp_apical
             {'params': lp_fname.parameters()},
             {'params': lp_varuse.parameters()},
             {'params': lp_apicall.parameters()},
-        ], lr=0.01
+        ], lr=0.001
     )
     # optimizer = torch.optim.Adagrad(
     #     [
