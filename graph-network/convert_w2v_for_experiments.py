c2e6dac5f942a803c6e67ac594dbb6a7ec91e78b
Vitaly R <mortiv16@gmail.com>
2020-08-12 12:34:30 +0300

    prepare kg training

---
+++ graph-network/convert_w2v_for_experiments.py
@@ -0,0 +1,71 @@
+from Embedder import Embedder
+import torch
+import os, sys
+import pandas as pd
+import numpy as np
+from Dataset import get_train_test_val_indices, SourceGraphDataset, load_data
+import pickle
+
+
+def isint(val):
+    try:
+        int(val)
+        return True
+    except:
+        return False
+
+def load_w2v(path):
+    id_map = {}
+    vecs = []
+    with open(emb_path) as vectors:
+        n_vectors, n_dims = map(int, vectors.readline().strip().split())
+
+        for ind in range(n_vectors):
+            elements = vectors.readline().strip().split()
+
+            if isint(elements[0]):
+                id_ = int(elements[0])
+            else:
+                continue
+
+            vec = list(map(float, elements[1:]))
+            assert len(vec) == n_dims
+            id_map[id_] = len(vecs)
+            vecs.append(vec)
+    vecs = np.array(vecs)
+    return id_map, vecs
+
+emb_path = sys.argv[1]
+nodes_path = sys.argv[2]
+edges_path = sys.argv[3]
+out_path = sys.argv[4]
+
+nodes, edges = load_data(nodes_path, edges_path)
+
+splits = get_train_test_val_indices(nodes.index)
+
+id_map, vecs = load_w2v(emb_path)
+
+nodes['global_graph_id'] = nodes['id'].apply(lambda x: id_map[x])
+
+# nodes, edges, held = SourceGraphDataset.holdout(nodes, edges, 0.001)
+# nodes['label'] = nodes['type']
+
+# emb = Embedder.load_word2vec(emb_path)
+emb = Embedder(id_map, vecs)
+
+if not os.path.isdir(out_path):
+    os.mkdir(out_path)
+
+torch.save(
+    {
+        "splits": splits
+    },
+    os.path.join(out_path, "state_dict.pt")
+)
+
+nodes.to_csv(os.path.join(out_path, "nodes.csv"), index=False)
+edges.to_csv(os.path.join(out_path, "edges.csv"), index=False)
+# held.to_csv(os.path.join(out_path,  "held.csv"), index=False)
+
+pickle.dump([emb], open(os.path.join(out_path, "embeddings.pkl"), "wb"))
---
+++ graph-network/prepare_dglke_format.py
@@ -0,0 +1,82 @@
+from Embedder import Embedder
+import torch
+import os, sys
+import pandas as pd
+from Dataset import get_train_test_val_indices, SourceGraphDataset, load_data, compact_property
+import pickle
+
+import argparse
+
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('--nodes_path', dest='nodes_path', default=None,
+                    help='Path to the file with nodes')
+parser.add_argument('--edges_path', dest='edges_path', default=None,
+                    help='Path to the file with edges')
+parser.add_argument('--fname_path', dest='fname_path', default=None,
+                    help='')
+parser.add_argument('--varuse_path', dest='varuse_path', default=None,
+                    help='')
+parser.add_argument('--apicall_path', dest='apicall_path', default=None,
+                    help='')
+parser.add_argument('--out_path', dest='out_path', default=None,
+                    help='')
+
+args = parser.parse_args()
+
+nodes, edges = load_data(args.nodes_path, args.edges_path)
+node2graph_id = compact_property(nodes['id'])
+nodes['global_graph_id'] = nodes['id'].apply(lambda x: node2graph_id[x])
+
+nodes, edges, held = SourceGraphDataset.holdout(nodes, edges, 0.005)
+edges.to_csv(os.path.join(args.out_path, "edges_train.csv"), index=False)
+held.to_csv(os.path.join(args.out_path, "held.csv"), index=False)
+
+edges = edges.astype({"src": 'str', "dst": "str", "type": 'str'})[['src', 'dst', 'type']]
+
+node_ids = set(nodes['id'].unique())
+
+if args.fname_path is not None:
+    fname = pd.read_csv(args.fname_path).astype({"src": 'int32', "dst": "str"})
+    fname['type'] = 'fname'
+
+    fname = fname[
+        fname['src'].apply(lambda x: x in node_ids)
+    ]
+
+    edges = pd.concat([edges, fname])
+
+if args.varuse_path is not None:
+    varuse = pd.read_csv(args.varuse_path).astype({"src": 'int32', "dst": "str"})
+    varuse['type'] = 'varuse'
+
+    varuse = varuse[
+        varuse['src'].apply(lambda x: x in node_ids)
+    ]
+
+    edges = pd.concat([edges, varuse])
+
+if args.apicall_path is not None:
+    apicall = pd.read_csv(args.apicall_path).astype({"src": 'int32', "dst": "int32"})
+    apicall['type'] = 'nextcall'
+
+    apicall = apicall[
+        apicall['src'].apply(lambda x: x in node_ids)
+    ]
+
+    edges = pd.concat([edges, apicall])
+
+# splits = get_train_test_val_indices(edges.index, train_frac=0.6)
+
+nodes['label'] = nodes['type']
+
+if not os.path.isdir(args.out_path):
+    os.mkdir(args.out_path)
+
+nodes.to_csv(os.path.join(args.out_path, "nodes.csv"), index=False)
+edges.to_csv(os.path.join(args.out_path, "edges_train_dglke.tsv"), index=False, header=False, sep="\t")
+edges[['src','dst']].to_csv(os.path.join(args.out_path, "edges_train_node2vec.csv"), index=False, header=False, sep=" ")
+# edges.iloc[splits[0]].to_csv(os.path.join(args.out_path, "edges_train.csv"), index=False, header=False, sep="\t")
+# edges.iloc[splits[1]].to_csv(os.path.join(args.out_path, "edges_val.csv"), index=False, header=False, sep="\t")
+# edges.iloc[splits[2]].to_csv(os.path.join(args.out_path, "edges_test.csv"), index=False, header=False, sep="\t")
+held[['src','dst','type']].to_csv(os.path.join(args.out_path, "held_dglkg.tsv"), index=False, sep='\t', header=False)
+held[['src','dst']].to_csv(os.path.join(args.out_path, "held_node2vec.csv"), index=False, sep=' ', header=False)
