85f44fd2f7cf93ff7c79c7dbcea9de54723f62f3
Vitaly R <mortiv16@gmail.com>
2020-08-27 12:34:04 +0300

    allow maps with missing nodes

--- SourceCodeTools/data/sourcetrail/sourcetrail-node-local2global.py
+++ SourceCodeTools/data/sourcetrail/sourcetrail-node-local2global.py
@@ -12,6 +12,6 @@ local_map_path = sys.argv[3]
 rev_id_map = dict(zip(all_nodes['serialized_name'].tolist(), all_nodes['id'].tolist()))
 id_map = dict(zip(orig_nodes["id"].tolist(), map(lambda x: rev_id_map[x], orig_nodes["serialized_name"].tolist())))
 
-orig_nodes['global_id'] = orig_nodes['id'].apply(lambda x: id_map[x])
+orig_nodes['global_id'] = orig_nodes['id'].apply(lambda x: id_map.get(x, -1))
 
 orig_nodes[['id', 'global_id']].to_csv(local_map_path, index=False)
