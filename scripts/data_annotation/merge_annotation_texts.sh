b1a999868376f52412bedf5eb88cf1c8b61a21f5
Vitaly R <mortiv16@gmail.com>
2020-09-10 17:17:18 +0300

    move local node mapping to the annotation extraction

--- scripts/data_annotation/merge_annotation_texts.sh
+++ scripts/data_annotation/merge_annotation_texts.sh
@@ -6,6 +6,7 @@ for dir in $(ls $ENVS_DIR); do
   if [ -d $ENVS_DIR/$dir ]; then
     echo "Process $dir"
     if [ -f $ENVS_DIR/$dir/normalized_sourcetrail_nodes.csv ]; then
+      sourcetrail-node-local2global.py $ENVS_DIR/common_nodes_with_ast.csv $ENVS_DIR/$dir/nodes_with_ast.csv $ENVS_DIR/$dir/local2global_with_ast.csv
       sourcecodetools-extract-type-annotations.py $ENVS_DIR/$dir/source-graph-bodies.csv $ENVS_DIR/functions_with_annotations.jsonl $ENVS_DIR/$dir/local2global.csv
     fi
   fi
