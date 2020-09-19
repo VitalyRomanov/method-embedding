7da12be309a335cf839e8e83057a37ff20d027de
Vitaly R <mortiv16@gmail.com>
2020-09-19 18:24:25 +0300

    removed  global variables

--- SourceCodeTools/data/sourcetrail/sourcetrail-ast-edges.py
+++ SourceCodeTools/data/sourcetrail/sourcetrail-ast-edges.py
@@ -6,6 +6,9 @@ import re
 from copy import copy
 import ast
 
+from SourceCodeTools.proc.entity.annotator.annotator_utils import to_offsets
+
+
 # from node_name_serializer import deserialize_node_name
 
 working_directory = sys.argv[1]
@@ -18,128 +21,207 @@ node = pd.read_csv(node_path, sep=",", dtype={"id": int, "type": int, "serialize
 edge = pd.read_csv(edge_path, sep=",", dtype={'id': int, 'type': int, 'source_node_id': int, 'target_node_id': int})
 bodies = pd.read_csv(bodies_path, sep=",", dtype={"id": int,"body": str,"docstring": str,"normalized_body": str})
 
-nodeid2name = dict(zip(node['id'].tolist(), node['serialized_name'].tolist()))
+class NodeResolver:
+    def __init__(self, node):
 
-valid_new_type = 260 # fits about 250 new type
-type_maps = {}
-new_types = []
+        self.nodeid2name = dict(zip(node['id'].tolist(), node['serialized_name'].tolist()))
 
-valid_new_node = node['id'].max() + 1
-ast_node_type = 3
-node_maps = {}
-new_nodes = []
+        # self.valid_new_type = 260 # fits about 250 new type
+        # self.type_maps = {}
+        # self.new_types = []
 
-def resolve_edge_type(edge_type):
-    global valid_new_type, type_maps, new_types
+        self.valid_new_node = node['id'].max() + 1
+        self.ast_node_type = 3
+        self.node_maps = {}
+        self.new_nodes = []
 
-    # if edge_type not in type_maps:
-    #     type_maps[edge_type] = valid_new_type
-    #     new_types.append({"type_id": valid_new_type, "type_desc": edge_type})
-    #     valid_new_type += 1
-    #     if valid_new_type == 512: raise Exception("Type overlap!")
-    return edge_type #type_maps[edge_type]
+    def resolve(self, name_orig):
 
+        name_ = copy(name_orig)
 
-def resolve_node_names(name_orig):
-    global valid_new_node, ast_node_type, node_maps, new_nodes
+        replacements = dict()
+        for name in re.finditer("srstrlnd_[0-9]+", name_):
+            if isinstance(name, re.Match):
+                name = name.group()
+            elif isinstance(name, str):
+                pass
+            else:
+                print("Unknown type")
+            if name.startswith("srstrlnd_"):
+                node_id = name.split("_")[1]
+                replacements[name] = {
+                    "name": self.nodeid2name[int(node_id)],
+                    "id": node_id
+                }
 
-    name_ = copy(name_orig)
+        # try to replace into node id
+        for r, v in replacements.items():
+            name_ = name_.replace(r, v["id"])
 
-    replacements = dict()
-    for name in re.finditer("srstrlnd_[0-9]+", name_):
-        if isinstance(name, re.Match):
-            name = name.group()
-        elif isinstance(name, str):
+        try:
+            node_id = int(name_)
+            return node_id
+        except:
+            # can go here if the node is not sourcetrail node or
+            # if the node is a composite type
             pass
-        else:
-            print("Unknown type")
-        if name.startswith("srstrlnd_"):
-            node_id = name.split("_")[1]
-            replacements[name] = {
-                "name": nodeid2name[int(node_id)],
-                "id": node_id
-            }
-            # if nodeid2name[int(node_id)]=="bokeh.io.output.output_file":
-            #     pass
 
-    # try to replace into node id
-    for r, v in replacements.items():
-        name_ = name_.replace(r, v["id"])
+        # failed to convert into id, create new node
+        name_ = copy(name_orig)
+        for r, v in replacements.items():
+            name_ = name_.replace(r, v["name"])
 
-    try:
-        node_id = int(name_)
-        return node_id
-    except:
-        pass
-
-    # failed to convert into id, create new node
-    name_ = copy(name_orig)
-    for r, v in replacements.items():
-        name_ = name_.replace(r, v["name"])
-
-    name = name_
-    if name not in node_maps:
-        node_maps[name] = valid_new_node
-        new_nodes.append({"id": valid_new_node, "type": ast_node_type, "serialized_name": name})
-        valid_new_node += 1
-    return node_maps[name]
-
-    # if len(replacements) == 1:
-    #     # this is an existing node
-    #     for r, v in replacements.items():
-    #         name_ = name_.replace(r, v["id"])
-    #     node_id = int(name_)
-    #     return node_id
-    # else:
-    #     # this is a new node, has either 0 or >=2 replacements
-    #     for r, v in replacements.items():
-    #         name_ = name_.replace(r, v["name"])
-    #
-    #     name = name_
-    #     if name not in node_maps:
-    #         node_maps[name] = valid_new_node
-    #         new_nodes.append({"id": valid_new_node, "type": ast_node_type, "serialized_name": name})
-    #         valid_new_node += 1
-    #     return node_maps[name]
+        name = name_
+        if name not in self.node_maps:
+            self.node_maps[name] = self.valid_new_node
+            self.new_nodes.append({"id": self.valid_new_node, "type": self.ast_node_type, "serialized_name": name})
+            self.valid_new_node += 1
+        return self.node_maps[name]
 
 
+# def resolve_edge_type(edge_type):
+#     global valid_new_type, type_maps, new_types
+#
+#     # if edge_type not in type_maps:
+#     #     type_maps[edge_type] = valid_new_type
+#     #     new_types.append({"type_id": valid_new_type, "type_desc": edge_type})
+#     #     valid_new_type += 1
+#     #     if valid_new_type == 512: raise Exception("Type overlap!")
+#     return edge_type #type_maps[edge_type]
+
+
+# def resolve_node_names(name_orig):
+#     global valid_new_node, ast_node_type, node_maps, new_nodes
+#
+#     name_ = copy(name_orig)
+#
+#     replacements = dict()
+#     for name in re.finditer("srstrlnd_[0-9]+", name_):
+#         if isinstance(name, re.Match):
+#             name = name.group()
+#         elif isinstance(name, str):
+#             pass
+#         else:
+#             print("Unknown type")
+#         if name.startswith("srstrlnd_"):
+#             node_id = name.split("_")[1]
+#             replacements[name] = {
+#                 "name": nodeid2name[int(node_id)],
+#                 "id": node_id
+#             }
+#             # if nodeid2name[int(node_id)]=="bokeh.io.output.output_file":
+#             #     pass
+#
+#     # try to replace into node id
+#     for r, v in replacements.items():
+#         name_ = name_.replace(r, v["id"])
+#
+#     try:
+#         node_id = int(name_)
+#         return node_id
+#     except:
+#         # can go here if the node is not sourcetrail node or
+#         # if the node is a composite type
+#         pass
+#
+#     # failed to convert into id, create new node
+#     name_ = copy(name_orig)
+#     for r, v in replacements.items():
+#         name_ = name_.replace(r, v["name"])
+#
+#     name = name_
+#     if name not in node_maps:
+#         node_maps[name] = valid_new_node
+#         new_nodes.append({"id": valid_new_node, "type": ast_node_type, "serialized_name": name})
+#         valid_new_node += 1
+#     return node_maps[name]
+#
+#     # if len(replacements) == 1:
+#     #     # this is an existing node
+#     #     for r, v in replacements.items():
+#     #         name_ = name_.replace(r, v["id"])
+#     #     node_id = int(name_)
+#     #     return node_id
+#     # else:
+#     #     # this is a new node, has either 0 or >=2 replacements
+#     #     for r, v in replacements.items():
+#     #         name_ = name_.replace(r, v["name"])
+#     #
+#     #     name = name_
+#     #     if name not in node_maps:
+#     #         node_maps[name] = valid_new_node
+#     #         new_nodes.append({"id": valid_new_node, "type": ast_node_type, "serialized_name": name})
+#     #         valid_new_node += 1
+#     #     return node_maps[name]
+
+def get_sourcetrail_nodes(edges):
+    nodes = []
+    for ind, row in edges.iterrows():
+        if pd.isna(row["line"]):
+            continue
+
+        if row['src'].startswith("srstrlnd_"):
+            nodes.append((
+                row['line'],
+                row['end_line'],
+                row['col_offset'],
+                row['end_col_offset'],
+                row['src']
+            ))
+
+    return nodes
+
+
+
+node_resolver = NodeResolver(node)
 edges_with_ast_name = os.path.join(working_directory, "edges_with_ast.csv")
 edge.to_csv(edges_with_ast_name, index=False, quoting=QUOTE_NONNUMERIC)
 
-for ind, c in enumerate(bodies['normalized_body']):
-#     c = """def _zip_axes_from_type(
-#     typ: Type[FrameOrSeries], new_axes: Sequence[int]
-# ) -> Dict[str, int]:
-#     axes = {name: new_axes[i] for i, name in typ._AXIS_NAMES.items()}
-#     return axes"""
-#     assert isinstance(c, str)
-    if not isinstance(c, str): continue
+for ind, (_, row) in enumerate(bodies.iterrows()):
+    c_ = row['normalized_body']
+    if not isinstance(c_, str): continue
 
-    c = c.strip()
+    c = c_.lstrip()
+    strip_len = len(c_) - len(c)
+
     try:
         ast.parse(c)
-    except SyntaxError:
+    except SyntaxError as e:
+        print(e)
         continue
 
-    g = AstGraphGenerator(c.strip())
+    g = AstGraphGenerator(c)
 
     edges = g.get_edges()
+
+    srctrlnodes = get_sourcetrail_nodes(edges)
 
     if len(edges) == 0:
         continue
 
-    # assert srstrlnd_2703[compressor].srstrlnd_4109 == srstrlnd_2697.frame.LZ4FrameFile
-    # try:
-    edges['type'] = edges['type'].apply(resolve_edge_type)
-    edges['source_node_id'] = edges['src'].apply(resolve_node_names)
-    edges['target_node_id'] = edges['dst'].apply(resolve_node_names)
+    # edges['type'] = edges['type'].apply(resolve_edge_type)
+    # edges['source_node_id'] = edges['src'].apply(resolve_node_names)
+    # edges['target_node_id'] = edges['dst'].apply(resolve_node_names)
+    edges['source_node_id'] = edges['src'].apply(node_resolver.resolve)
+    edges['target_node_id'] = edges['dst'].apply(node_resolver.resolve)
     edges['id'] = 0
 
-    # except:
-    #     print(c)
-    #     print(edges)
-    #     raise Exception()
-        # continue
+    replacements = []
+    for ind, row in edges.iterrows():
+        if pd.isna(row['line']):
+            continue
+        replacements.append((
+            row['line'],
+            row['end_line'],
+            row['col_offset'],
+            row['end_col_offset'],
+            row['source_node_id']
+        ))
+
+    replacements_offsets = to_offsets(c, replacements)
+
+    if strip_len > 0:
+        replacements_offsets = list(map(lambda x: (x[0] + strip_len, x[1] + strip_len, x[2]), replacements_offsets))
 
     edges[['id','type','source_node_id','target_node_id']].to_csv(edges_with_ast_name, mode="a", index=False, header=False)
     print("\r%d/%d" % (ind, len(bodies['normalized_body'])), end="")
@@ -149,5 +231,5 @@ print(" " * 30 , end = "\r")
 
 
 with open(os.path.join(working_directory, "nodes_with_ast.csv"), 'w', encoding='utf8', errors='replace') as f:
-    pd.concat([node, pd.DataFrame(new_nodes)]).to_csv(f, index=False, quoting=QUOTE_NONNUMERIC)
+    pd.concat([node, pd.DataFrame(node_resolver.new_nodes)]).to_csv(f, index=False, quoting=QUOTE_NONNUMERIC)
 # pd.concat([node, pd.DataFrame(new_nodes)]).to_csv(os.path.join(working_directory, "nodes_with_ast.csv"), index=False)
