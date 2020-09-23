d3d11adc17524c161e08e1067bcdea8dc87253c4
Vitaly R <mortiv16@gmail.com>
2020-09-23 11:32:31 +0300

    polish extraction

--- scripts/data_extraction/process_sourcetrail.sh
+++ scripts/data_extraction/process_sourcetrail.sh
@@ -1,7 +1,7 @@
 conda activate SourceCodeTools
 
 ENVS_DIR=$1
-RUN_DIR=$(pwd)
+RUN_DIR=$(dirname "$0")
 SQL_Q=$RUN_DIR/extract.sql
 
 #echo $RUN_DIR
@@ -13,40 +13,39 @@ SQL_Q=$RUN_DIR/extract.sql
 
 for dir in $(ls $ENVS_DIR); do
   if [ -d $ENVS_DIR/$dir ]; then
-    cd $ENVS_DIR/$dir
     echo "Found package $dir"
 
-
-#    if [ -f $ENVS_DIR/$dir/source-graph-bodies.csv ]; then
-#      rm $ENVS_DIR/$dir/source-graph-bodies.csv
-#    fi
+    if [ -f $ENVS_DIR/$dir/source-graph-bodies.csv ]; then
+      rm $ENVS_DIR/$dir/source-graph-bodies.csv
+    fi
     if [ -f $ENVS_DIR/$dir/nodes_with_ast.csv ]; then
       rm $ENVS_DIR/$dir/nodes_with_ast.csv
     fi
     if [ -f $ENVS_DIR/$dir/edges_with_ast.csv ]; then
       rm $ENVS_DIR/$dir/edges_with_ast.csv
     fi
-#    if [ -f $ENVS_DIR/$dir/call_seq.csv ]; then
-#      rm $ENVS_DIR/$dir/call_seq.csv
-#    fi
-#    if [ -f $ENVS_DIR/$dir/source-graph-function-variable-pairs.csv ]; then
-#      rm $ENVS_DIR/$dir/source-graph-function-variable-pairs.csv
-#    fi
+    if [ -f $ENVS_DIR/$dir/call_seq.csv ]; then
+      rm $ENVS_DIR/$dir/call_seq.csv
+    fi
+    if [ -f $ENVS_DIR/$dir/source-graph-function-variable-pairs.csv ]; then
+      rm $ENVS_DIR/$dir/source-graph-function-variable-pairs.csv
+    fi
 
 
-    if [ -f $dir.srctrldb ]; then
-#      sqlite3 $dir.srctrldb < $SQL_Q
+    if [ -f $ENVS_DIR/$dir/$dir.srctrldb ]; then
+      cd $ENVS_DIR/$dir
+      sqlite3 $ENVS_DIR/$dir/$dir.srctrldb < $SQL_Q
       cd $RUN_DIR
-#      sourcetrail-verify-files.py $ENVS_DIR/$dir
-#      sourcetrail-node-name-merge.py $ENVS_DIR/$dir/nodes.csv
-#      sourcetrail-parse-bodies.py $ENVS_DIR/$dir
+      sourcetrail-verify-files.py $ENVS_DIR/$dir
+      sourcetrail-node-name-merge.py $ENVS_DIR/$dir/nodes.csv
+      sourcetrail-parse-bodies.py $ENVS_DIR/$dir
       sourcetrail-ast-edges.py $ENVS_DIR/$dir
-#      sourcetrail-call-seq-extractor.py $ENVS_DIR/$dir
-#      sourcetrail-extract-variable-names.py py $ENVS_DIR/$dir
+      sourcetrail-call-seq-extractor.py $ENVS_DIR/$dir
+      sourcetrail-extract-variable-names.py py $ENVS_DIR/$dir
     else
       echo "Package not indexed"
     fi
   fi
 done
 
-#conda deactivate
+conda deactivate
