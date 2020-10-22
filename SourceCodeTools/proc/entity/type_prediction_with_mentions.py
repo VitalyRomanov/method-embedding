3809873e78c964d19c9e2c5ed70345df27c2d301
Vitaly R <mortiv16@gmail.com>
2020-10-22 19:17:27 +0300

    change hyperparams

--- SourceCodeTools/proc/entity/type_prediction_with_mentions.py
+++ SourceCodeTools/proc/entity/type_prediction_with_mentions.py
@@ -206,8 +206,18 @@ def main_tf_hyper_search(TRAIN_DATA, TEST_DATA,
     params = {
         "params": [
             {
-                "h_sizes": [20, 20, 20],
-                "dense_size": 20,
+                "h_sizes": [10, 10, 10],
+                "dense_size": 10,
+                "pos_emb_size": 10,
+                "cnn_win_size": 3,
+                "suffix_prefix_dims": 10,
+                "suffix_prefix_buckets": 1000,
+                "target_emb_dim": 5,
+                "mention_emb_dim": 5
+            },
+            {
+                "h_sizes": [20, 20],
+                "dense_size": 10,
                 "pos_emb_size": 20,
                 "cnn_win_size": 3,
                 "suffix_prefix_dims": 20,
@@ -215,28 +225,48 @@ def main_tf_hyper_search(TRAIN_DATA, TEST_DATA,
                 "target_emb_dim": 5,
                 "mention_emb_dim": 5
             },
-            {
-                "h_sizes": [40, 40, 40],
-                "dense_size": 30,
-                "pos_emb_size": 30,
-                "cnn_win_size": 5,
-                "suffix_prefix_dims": 50,
-                "suffix_prefix_buckets": 2000,
-                "target_emb_dim": 15,
-                "mention_emb_dim": 15
-            },
-            {
-                "h_sizes": [80, 80, 80],
-                "dense_size": 40,
-                "pos_emb_size": 50,
-                "cnn_win_size": 7,
-                "suffix_prefix_dims": 70,
-                "suffix_prefix_buckets": 3000,
-                "target_emb_dim": 25,
-                "mention_emb_dim": 25,
-            }
+            # {
+            #     "h_sizes": [20, 20, 20],
+            #     "dense_size": 20,
+            #     "pos_emb_size": 20,
+            #     "cnn_win_size": 3,
+            #     "suffix_prefix_dims": 20,
+            #     "suffix_prefix_buckets": 1000,
+            #     "target_emb_dim": 5,
+            #     "mention_emb_dim": 5
+            # },
+            # {
+            #     "h_sizes": [20, 20, 20],
+            #     "dense_size": 20,
+            #     "pos_emb_size": 20,
+            #     "cnn_win_size": 3,
+            #     "suffix_prefix_dims": 20,
+            #     "suffix_prefix_buckets": 1000,
+            #     "target_emb_dim": 5,
+            #     "mention_emb_dim": 5
+            # },
+            # {
+            #     "h_sizes": [40, 40, 40],
+            #     "dense_size": 30,
+            #     "pos_emb_size": 30,
+            #     "cnn_win_size": 5,
+            #     "suffix_prefix_dims": 50,
+            #     "suffix_prefix_buckets": 2000,
+            #     "target_emb_dim": 15,
+            #     "mention_emb_dim": 15
+            # },
+            # {
+            #     "h_sizes": [80, 80, 80],
+            #     "dense_size": 40,
+            #     "pos_emb_size": 50,
+            #     "cnn_win_size": 7,
+            #     "suffix_prefix_dims": 70,
+            #     "suffix_prefix_buckets": 3000,
+            #     "target_emb_dim": 25,
+            #     "mention_emb_dim": 25,
+            # }
         ],
-        "learning_rate": [0.0001,],
+        "learning_rate": [0.001,],
         "learning_rate_decay": [0.998] # 0.991
     }
     from sklearn.model_selection import ParameterGrid
