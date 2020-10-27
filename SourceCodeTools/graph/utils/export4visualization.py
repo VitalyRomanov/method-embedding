74e3c041b90cb905ab43f6924cff2b4817aa842f
Vitaly R <mortiv16@gmail.com>
2020-10-27 18:30:59 +0300

    export embeddings by types

--- SourceCodeTools/graph/utils/export4visualization.py
+++ SourceCodeTools/graph/utils/export4visualization.py
@@ -37,37 +37,51 @@ ids, names = zip(*id_name_map)
 
 print(f"Limiting to {max_embs} embeddings")
 
-names = []
-emb0 = []
-emb1 = []
-emb2 = []
 
-for ind, (id_, count) in enumerate(degrees.most_common()):
-    names.append(id_name_map_d[id_])
-    emb0.append(embedders[0].e[embedders[0].ind[id_]])
-    emb1.append(embedders[1].e[embedders[1].ind[id_]])
-    emb2.append(embedders[2].e[embedders[2].ind[id_]])
-    if ind >= max_embs-1: break
+# emb0 = []
+# emb1 = []
+# emb2 = []
 
-# np.savetxt(os.path.join(model_path, "emb4proj_meta.tsv"), np.array(names).reshape(-1, 1))
-np.savetxt(os.path.join(model_path, "emb4proj0.tsv"), np.array(emb0), delimiter="\t")
-np.savetxt(os.path.join(model_path, "emb4proj1.tsv"), np.array(emb1), delimiter="\t")
-np.savetxt(os.path.join(model_path, "emb4proj2.tsv"), np.array(emb2), delimiter="\t")
+for group, gr_ in nodes.groupby("type_backup"):
+    c_ = 0
 
-print("Writing meta...", end="")
-with open(os.path.join(model_path, "emb4proj_meta.tsv"), "w") as meta:
-    for name in names[:max_embs]:
-        meta.write(f"{name}\n")
-print("done")
+    names = []
+    embs = [[] for _ in embedders]
 
-with open(os.path.join(model_path, "emb4proj2w2v.txt"), "w") as w2v:
-    for ind, name in enumerate(names):
-        w2v.write("%s " % name)
-        for j, v in enumerate(emb2[ind]):
-            if j < len(emb2[ind]) - 1:
-                w2v.write("%f " % v)
-            else:
-                w2v.write("%f\n" % v)
+    nodes_in_group = set(gr_['id'].tolist())
+    for ind, (id_, count) in enumerate(degrees.most_common()):
+        if id_ not in nodes_in_group: continue
+        names.append(id_name_map_d[id_])
+        for emb_, embedder in zip(embs, embedders):
+            emb_.append(embedder.e[embedder.ind[id_]])
+        # emb0.append(embedders[0].e[embedders[0].ind[id_]])
+        # emb1.append(embedders[1].e[embedders[1].ind[id_]])
+        # emb2.append(embedders[2].e[embedders[2].ind[id_]])
+        c_ += 1
+        if c_ >= max_embs - 1: break
+        # if ind >= max_embs-1: break
+
+    # np.savetxt(os.path.join(model_path, "emb4proj_meta.tsv"), np.array(names).reshape(-1, 1))
+    for ind, emb_ in enumerate(embs):
+        np.savetxt(os.path.join(model_path, f"emb4proj{ind}_{group}.tsv"), np.array(emb_), delimiter="\t")
+    # np.savetxt(os.path.join(model_path, "emb4proj0.tsv"), np.array(emb0), delimiter="\t")
+    # np.savetxt(os.path.join(model_path, "emb4proj1.tsv"), np.array(emb1), delimiter="\t")
+    # np.savetxt(os.path.join(model_path, "emb4proj2.tsv"), np.array(emb2), delimiter="\t")
+
+    print("Writing meta...", end="")
+    with open(os.path.join(model_path, f"emb4proj_meta_{group}.tsv"), "w") as meta:
+        for name in names[:max_embs]:
+            meta.write(f"{name}\n")
+    print("done")
+
+# with open(os.path.join(model_path, "emb4proj2w2v.txt"), "w") as w2v:
+#     for ind, name in enumerate(names):
+#         w2v.write("%s " % name)
+#         for j, v in enumerate(emb2[ind]):
+#             if j < len(emb2[ind]) - 1:
+#                 w2v.write("%f " % v)
+#             else:
+#                 w2v.write("%f\n" % v)
 
 
 # for ind, e in enumerate(embedders):
