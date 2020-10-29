f752821bc7b087ffa4a2e6143db8e248b448bfd4
Vitaly R <mortiv16@gmail.com>
2020-10-29 23:29:42 +0300

    disable attention, change parameters

--- SourceCodeTools/proc/entity/type_prediction.py
+++ SourceCodeTools/proc/entity/type_prediction.py
@@ -550,14 +550,14 @@ def main_tf_hyper_search(TRAIN_DATA, TEST_DATA,
             #     "suffix_prefix_dims": 10,
             #     "suffix_prefix_buckets": 1000,
             # },
-            {
-                "h_sizes": [20, 20, 20],
-                "dense_size": 20,
-                "pos_emb_size": 20,
-                "cnn_win_size": 5,
-                "suffix_prefix_dims": 20,
-                "suffix_prefix_buckets": 1000,
-            },
+            # {
+            #     "h_sizes": [20, 20, 20],
+            #     "dense_size": 20,
+            #     "pos_emb_size": 20,
+            #     "cnn_win_size": 5,
+            #     "suffix_prefix_dims": 20,
+            #     "suffix_prefix_buckets": 1000,
+            # },
             {
                 "h_sizes": [40, 40, 40],
                 "dense_size": 30,
@@ -581,7 +581,7 @@ def main_tf_hyper_search(TRAIN_DATA, TEST_DATA,
         # "cnn_win_size": [3, 5, 7],
         # "suffix_prefix_dims": [20, 50, 70],
         # "suffix_prefix_buckets": [1000, 2000],
-        "learning_rate": [0.0001, 0.001],
+        "learning_rate": [0.0001],
         "learning_rate_decay": [0.998] # 0.991
     }
     from sklearn.model_selection import ParameterGrid
@@ -627,6 +627,8 @@ def main_tf_hyper_search(TRAIN_DATA, TEST_DATA,
             with open(os.path.join(trial_dir, "params.json"), "w") as metadata_sink:
                 metadata_sink.write(json.dumps(metadata, indent=4))
 
+            pickle.dump((t_map, inv_t_map), open(os.path.join(trial_dir, "tag_types.json"), "wb"))
+
 
 
 def main_tf(TRAIN_DATA, TEST_DATA,
@@ -658,6 +660,8 @@ def main_tf(TRAIN_DATA, TEST_DATA,
           scorer=lambda pred, true: scorer(pred, true, inv_t_map), learning_rate_decay=learning_rate_decay, finetune=finetune)
 
     model.save_weights(output_dir)
+
+    pickle.dump((t_map, inv_t_map), open("tag_types.pkl", "wb"))
 
 
 
