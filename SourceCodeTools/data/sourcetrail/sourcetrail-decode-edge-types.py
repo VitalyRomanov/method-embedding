c9d75c3e49889fd1e0733de8b007a3684386fa0d
Vitaly R <mortiv16@gmail.com>
2021-01-02 13:21:11 +0300

    script for decoding sourcetrail edge types

---
+++ SourceCodeTools/data/sourcetrail/sourcetrail-decode-edge-types.py
@@ -0,0 +1,17 @@
+from SourceCodeTools.data.sourcetrail.sourcetrail_types import edge_types
+from SourceCodeTools.data.sourcetrail.file_utils import *
+
+import pandas as p
+import sys
+import os
+
+
+edges_path = sys.argv[1]
+
+
+edges = p.read_csv(edges_path, dtype={"id": int, "type": int, "source_node_id": int, "target_node_id": int})
+edges['type'] = edges['type'].apply(lambda x: edge_types[x])
+
+edges = edges.astype({"id": int, "type": str, "source_node_id": int, "target_node_id": int})
+
+persist(edges, os.path.join(os.path.dirname(edges_path), filenames["edges"]))
