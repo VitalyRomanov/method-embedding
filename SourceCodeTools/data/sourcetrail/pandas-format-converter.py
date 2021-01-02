9195e75b6c3ceb827765355f12c17850d6ed16d2
Vitaly R <mortiv16@gmail.com>
2021-01-02 13:19:05 +0300

    script for converting between pands formats

---
+++ SourceCodeTools/data/sourcetrail/pandas-format-converter.py
@@ -0,0 +1,12 @@
+import sys
+from SourceCodeTools.data.sourcetrail.file_utils import *
+
+input_path = sys.argv[1]
+target_format = sys.argv[2]
+
+base_dir = os.path.dirname(input_path)
+filename = os.path.basename(input_path)
+output_path = os.path.join(base_dir, ".".join(filename.split(".")[:-1]) + "." + target_format)
+
+data = unpersist(input_path)
+persist(data, output_path)
