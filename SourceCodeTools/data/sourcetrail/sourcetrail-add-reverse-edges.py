14126b844aea0618848e52096d0e44b52c67e0bc
Vitaly R <mortiv16@gmail.com>
2021-01-02 13:20:34 +0300

    script for adding reverse edges

---
+++ SourceCodeTools/data/sourcetrail/sourcetrail-add-reverse-edges.py
@@ -0,0 +1,23 @@
+from SourceCodeTools.data.sourcetrail.sourcetrail_types import edge_types
+from SourceCodeTools.data.sourcetrail.file_utils import *
+
+import pandas as p
+import sys
+import os
+
+
+def add_reverse_edges(edges):
+    rev_edges = edges.copy()
+    rev_edges['source_node_id'] = edges['target_node_id']
+    rev_edges['target_node_id'] = edges['source_node_id']
+    rev_edges['type'] = rev_edges['type'].apply(lambda x: x + "_rev")
+
+    return p.concat([edges, rev_edges], axis=0)
+
+
+edges_path = sys.argv[1]
+
+edges = unpersist(edges_path)
+edges = add_reverse_edges(edges)
+
+persist(edges, edges_path)
