d44faafc46eb0b40ac768dddbc4b4277a86bee58
Vitaly R <mortiv16@gmail.com>
2021-01-22 02:07:45 +0300

    removed function for creating element embedders

--- SourceCodeTools/graph/model/train/utils.py
+++ SourceCodeTools/graph/model/train/utils.py
@@ -43,41 +43,41 @@ def evaluate_no_classes(logits, labels):
     return acc
 
 
-def create_elem_embedder(element_data, nodes, emb_size, compact_dst):
-    # element_data = unpersist(file_path)
-
-    if len(element_data) == 0:
-        logging.error(f"Not enough data for the embedder: {len(element_data)}. Exiting...")
-        sys.exit()
-
-    id2nodeid = dict(zip(nodes['id'].tolist(), nodes['global_graph_id'].tolist()))
-    id2typedid = dict(zip(nodes['id'].tolist(), nodes['typed_id'].tolist()))
-    id2type = dict(zip(nodes['id'].tolist(), nodes['type'].tolist()))
-
-    element_data['id'] = element_data['src'].apply(lambda x: id2nodeid.get(x, None))
-    element_data['src_type'] = element_data['src'].apply(lambda x: id2type.get(x, None))
-    element_data['src_typed_id'] = element_data['src'].apply(lambda x: id2typedid.get(x, None))
-    element_data = element_data.astype({
-        'id': 'Int32',
-        'src_type': 'category',
-        'src_typed_id': 'Int32',
-    })
-
-    if compact_dst is False:  # creating api call embedder
-        element_data = element_data.rename({'dst': 'dst_orig'}, axis=1)
-        element_data['dst'] = element_data['dst_orig'].apply(lambda x: id2nodeid.get(x, None))
-        element_data['dst_type'] = element_data['dst_orig'].apply(lambda x: id2type.get(x, None))
-        element_data['dst_typed_id'] = element_data['dst_orig'].apply(lambda x: id2typedid.get(x, None))
-        element_data.drop_duplicates(['id', 'dst'], inplace=True, ignore_index=True)  # this line apparenly filters parallel edges
-        element_data = element_data.astype({
-            'dst': 'Int32',
-            'dst_type': 'category',
-            'dst_typed_id': 'Int32',
-        })
-
-    element_data = element_data.dropna(axis=0)
-    ee = ElementEmbedder(element_data, emb_size, compact_dst=compact_dst)
-    return ee
+# def create_elem_embedder(element_data, nodes, emb_size, compact_dst):
+#     # element_data = unpersist(file_path)
+#
+#     if len(element_data) == 0:
+#         logging.error(f"Not enough data for the embedder: {len(element_data)}. Exiting...")
+#         sys.exit()
+#
+#     id2nodeid = dict(zip(nodes['id'].tolist(), nodes['global_graph_id'].tolist()))
+#     id2typedid = dict(zip(nodes['id'].tolist(), nodes['typed_id'].tolist()))
+#     id2type = dict(zip(nodes['id'].tolist(), nodes['type'].tolist()))
+#
+#     element_data['id'] = element_data['src'].apply(lambda x: id2nodeid.get(x, None))
+#     element_data['src_type'] = element_data['src'].apply(lambda x: id2type.get(x, None))
+#     element_data['src_typed_id'] = element_data['src'].apply(lambda x: id2typedid.get(x, None))
+#     element_data = element_data.astype({
+#         'id': 'Int32',
+#         'src_type': 'category',
+#         'src_typed_id': 'Int32',
+#     })
+#
+#     if compact_dst is False:  # creating api call embedder
+#         element_data = element_data.rename({'dst': 'dst_orig'}, axis=1)
+#         element_data['dst'] = element_data['dst_orig'].apply(lambda x: id2nodeid.get(x, None))
+#         element_data['dst_type'] = element_data['dst_orig'].apply(lambda x: id2type.get(x, None))
+#         element_data['dst_typed_id'] = element_data['dst_orig'].apply(lambda x: id2typedid.get(x, None))
+#         element_data.drop_duplicates(['id', 'dst'], inplace=True, ignore_index=True)  # this line apparenly filters parallel edges
+#         element_data = element_data.astype({
+#             'dst': 'Int32',
+#             'dst_type': 'category',
+#             'dst_typed_id': 'Int32',
+#         })
+#
+#     element_data = element_data.dropna(axis=0)
+#     ee = ElementEmbedder(element_data, emb_size, compact_dst=compact_dst)
+#     return ee
 
 
 class BestScoreTracker:
