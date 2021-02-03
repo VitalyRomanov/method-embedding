39e10ef488a3d4944f027040522187842b554590
Vitaly R <mortiv16@gmail.com>
2021-02-03 20:47:06 +0300

    create callable function

--- SourceCodeTools/code/data/sourcetrail/sourcetrail_draw_graph.py
+++ SourceCodeTools/code/data/sourcetrail/sourcetrail_draw_graph.py
@@ -1,28 +1,34 @@
 import pandas as pd
 import pygraphviz as pgv
 
-from SourceCodeTools.code.data.sourcetrail.sourcetrail_types import node_types
 
-global_types = list(node_types.values())
+def visualize(nodes, edges, output_path):
+    from SourceCodeTools.code.data.sourcetrail.sourcetrail_types import node_types
 
-nodes = pd.read_pickle("common_nodes.bz2")
-edges = pd.read_pickle("common_edges.bz2")
+    global_types = list(node_types.values())
 
-id2name = dict(zip(nodes['id'], nodes['serialized_name']))
-id2type = dict(zip(nodes['id'], nodes['type']))
+    edges = edges[edges["type"].apply(lambda x: not x.endswith("_rev"))]
 
-G=pgv.AGraph(strict=False,directed=True)
+    id2name = dict(zip(nodes['id'], nodes['serialized_name']))
+    id2type = dict(zip(nodes['id'], nodes['type']))
 
-for ind, edge in edges.iterrows():
-    src = edge['source_node_id']
-    dst = edge['target_node_id']
-    src_name = id2name[src]
-    dst_name = id2name[dst]
-    G.add_node(src_name, color="blue" if id2type[src] in global_types else "black")
-    G.add_node(dst_name, color="blue" if id2type[dst] in global_types else "black")
-    G.add_edge(src_name, dst_name)
-    g_edge = G.get_edge(src_name, dst_name)
-    g_edge.attr['label'] = edge['type']
+    g = pgv.AGraph(strict=False, directed=True)
 
-G.layout("dot")
-G.draw('file.pdf')
+    for ind, edge in edges.iterrows():
+        src = edge['source_node_id']
+        dst = edge['target_node_id']
+        src_name = id2name[src]
+        dst_name = id2name[dst]
+        g.add_node(src_name, color="blue" if id2type[src] in global_types else "black")
+        g.add_node(dst_name, color="blue" if id2type[dst] in global_types else "black")
+        g.add_edge(src_name, dst_name)
+        g_edge = g.get_edge(src_name, dst_name)
+        g_edge.attr['label'] = edge['type']
+
+    g.layout("dot")
+    g.draw(output_path)
+
+
+if __name__ == "__main__":
+    nodes = pd.read_pickle("common_nodes.bz2")
+    edges = pd.read_pickle("common_edges.bz2")
