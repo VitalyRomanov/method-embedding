79fb75eb9ee8dd0e03ac671e3cad052f42895a5c
Vitaly R <mortiv16@gmail.com>
2021-02-15 14:33:22 +0300

    add infor for more flexible op tokenization

--- SourceCodeTools/code/python_tokens_to_bpe_subwords.py
+++ SourceCodeTools/code/python_tokens_to_bpe_subwords.py
@@ -1,4 +1,6 @@
 # "▁"
+from functools import lru_cache
+
 python_ops_to_bpe = {
     'Add': ["▁+"],
     'Mod': ["▁%"],
@@ -22,10 +24,10 @@ python_ops_to_bpe = {
     'Pow': ["▁**"],
     'FloorDiv': ["▁//"],
     'GtE': ["▁>="],
-    # 'USub':
+    'USub': ["▁-"],
     'Invert': ["▁~"],
-    # 'UAdd'
-    # 'MatMul??'
+    'UAdd': ["▁+"],
+    'MatMult': ["▁@"],
     'BitXor': ["▁^"],
     'LShift': ["▁<<"],
     'RShift': ["▁>>"],
@@ -34,5 +36,44 @@ python_ops_to_bpe = {
     "Continue": ["▁continue"],
 }
 
+python_ops_to_literal = {
+    'Add': "+",
+    'Mod': "%",
+    'NotIn': "not in",
+    'And': "and",
+    'IsNot': "is not",
+    'Or': "or",
+    'Is': "is",
+    'Eq': "==",
+    'Gt': ">",
+    'NotEq': "!=",
+    'Not': "not",
+    'In': "in",
+    'BitAnd': "&",
+    'Sub': "-",
+    'Mult': "*",
+    'BitOr': "|",
+    'Lt': "<",
+    'Div': "/",
+    'LtE': "<=",
+    'Pow': "**",
+    'FloorDiv': "//",
+    'GtE': ">=",
+    'USub': "-", # this appearsto be the operator to chenge number sign -5
+    'Invert': "~",
+    'UAdd': "+", # this appearsto be the operator to chenge number sign +5, method __pos__
+    'MatMult': "@",
+    'BitXor': "^",
+    'LShift': "<<",
+    'RShift': ">>",
+    "Break": "break",
+    "Pass": "pass",
+    "Continue": "continue",
+}
+
 def op_tokenizer(op: str):
     return python_ops_to_bpe.get(op, op)
+
+@lru_cache
+def op_tokenize_or_none(op, tokenize_func):
+    return tokenize_func(python_ops_to_literal[op]) if op in python_ops_to_literal else None
