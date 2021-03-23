dce45a8b3f812a3b8c81d2efd9a9706a29867427
Vitaly R <mortiv16@gmail.com>
2021-03-23 23:13:15 +0300

    implemented scorer, now can do negative sampling based on the distance

--- SourceCodeTools/models/graph/ElementEmbedder.py
+++ SourceCodeTools/models/graph/ElementEmbedder.py
@@ -1,3 +1,4 @@
+import random
 from collections import Iterable
 
 import torch
@@ -10,6 +11,23 @@ from sklearn.neighbors._ball_tree import BallTree
 from sklearn.preprocessing import normalize
 
 from SourceCodeTools.models.graph.ElementEmbedderBase import ElementEmbedderBase
+from SourceCodeTools.models.graph.train.Scorer import Scorer
+
+
+class GraphLinkSampler(ElementEmbedderBase, Scorer):
+    def __init__(self, elements, nodes, compact_dst=True, dst_to_global=True, emb_size=None):
+        assert emb_size is not None
+        ElementEmbedderBase.__init__(self, elements=elements, nodes=nodes, compact_dst=compact_dst, dst_to_global=dst_to_global)
+        Scorer.__init__(self, num_embs=len(self.elements["dst"].unique()), emb_size=emb_size, src2dst=self.element_lookup)
+
+    def sample_negative(self, size, ids=None, strategy="closest"):
+        if strategy == "w2v":
+            negative = ElementEmbedderBase.sample_negative(self, size)
+        else:
+            negative = Scorer.sample_closest_negative(self, ids, k=size // len(ids))
+            assert len(negative) == size
+        return negative
+
 
 class ElementEmbedder(ElementEmbedderBase, nn.Module):
     def __init__(self, elements, nodes, emb_size, compact_dst=True):
@@ -48,22 +66,27 @@ from SourceCodeTools.nlp import token_hasher
 from SourceCodeTools.nlp.embed.fasttext import char_ngram_window
 
 
-class ElementEmbedderWithCharNGramSubwords(ElementEmbedderBase, nn.Module):
+class ElementEmbedderWithCharNGramSubwords(ElementEmbedderBase, nn.Module, Scorer):
     def __init__(self, elements, nodes, emb_size, num_buckets=5000, max_len=100, gram_size=3):
         ElementEmbedderBase.__init__(self, elements=elements, nodes=nodes, compact_dst=False)
         nn.Module.__init__(self)
+        Scorer.__init__(self, num_embs=len(self.elements["dst"].unique()), emb_size=emb_size,
+                        src2dst=self.element_lookup)
 
+        self.gram_size = gram_size
         self.emb_size = emb_size
+        self.init_subwords(elements, num_buckets=num_buckets, max_len=max_len)
 
+    def init_subwords(self, elements, num_buckets, max_len):
         names = elements['dst']
-        reprs = names.map(lambda x: char_ngram_window(x, gram_size)) \
+        reprs = names.map(lambda x: char_ngram_window(x, self.gram_size)) \
             .map(lambda grams: (token_hasher(g, num_buckets) for g in grams)) \
-            .map(lambda int_grams: np.fromiter(int_grams, dtype=np.int32))\
+            .map(lambda int_grams: np.fromiter(int_grams, dtype=np.int32)) \
             .map(lambda parts: create_fixed_length(parts, max_len, 0))
 
         self.name2repr = dict(zip(names, reprs))
 
-        self.embed = nn.Embedding(num_buckets, emb_size, padding_idx=0)
+        self.embed = nn.Embedding(num_buckets, self.emb_size, padding_idx=0)
 
     def __getitem__(self, ids):
         """
@@ -75,109 +98,116 @@ class ElementEmbedderWithCharNGramSubwords(ElementEmbedderBase, nn.Module):
         emb_matr = np.array([self.name2repr[c] for c in candidates], dtype=np.int32)
         return torch.LongTensor(emb_matr)
 
-    def sample_negative(self, size):
+    def sample_negative(self, size, ids=None, strategy="closest"):
         # TODO
         # Try other distributions
-        emb_matr = np.array([self.name2repr[c] for c in np.random.choice(self.idxs, size, replace=True, p=self.neg_prob)])
+        if strategy == "w2v":
+            negative = ElementEmbedderBase.sample_negative(self, size)
+        else:
+            ### negative = random.choices(Scorer.sample_closest_negative(self, ids), k=size)
+            negative = Scorer.sample_closest_negative(self, ids, k=size // len(ids))
+            assert len(negative) == size
+
+        emb_matr = np.array([self.name2repr[c] for c in negative])
         return torch.LongTensor(emb_matr)
 
     def forward(self, input, **kwargs):
         x = self.embed(input)
         return torch.mean(x, dim=1)
 
+    def set_embed(self):
+        all_keys = self.get_keys_for_scoring()
+        emb_matr = np.array([self.name2repr[key] for key in all_keys], dtype=np.int32)
+        with torch.set_grad_enabled(False):
+            self.scorer_all_emb = self(torch.LongTensor(emb_matr).to(self.embed.weight.device)).detach().numpy()
+
     def prepare_index(self):
-        all_keys = [key for key in self.name2repr]
-        emb_matr = np.array([self.name2repr[key] for key in all_keys], dtype=np.int32)
-        all_emb = self(torch.LongTensor(emb_matr)).detach().numpy()
-        self.all_emb = normalize(all_emb, axis=1)
-        self.ball_tree = BallTree(all_emb)
+        self.set_embed()
+        Scorer.prepare_index(self)
 
-    def score_candidates_cosine(self, to_score_ids, to_score_embs, at=None):
-        if at is None:
-            at = [1, 3, 5, 10]
+    # def score_candidates_cosine(self, to_score_ids, to_score_embs, keys_to_score_against, embs_to_score_against, at=None):
+    #
+    #     # y_pred = []
+    #     # to_score_embs = to_score_embs.detach().numpy()
+    #     # to_score_embs = normalize(to_score_embs, axis=1)
+    #     # for ind in range(to_score_embs.shape[0]):
+    #     #     curr_to_score = to_score_embs[ind].reshape(1, -1)
+    #     #     _, closest = self.ball_tree.query(curr_to_score, k=at if type(at) is int else max(at))
+    #     #     scores = (curr_to_score @ self.all_emb[closest.ravel(), :].T).ravel()
+    #     #     closest = closest.ravel().tolist()
+    #     #     y_pred.append([])
+    #     #     for i in range(len(keys_to_score_against)):
+    #     #         if i in closest:
+    #     #             y_pred[-1].append(scores[closest.index(i)])
+    #     #         else:
+    #     #             y_pred[-1].append(-1.)
+    #
+    #     score_matr = (to_score_embs @ embs_to_score_against.t()) / \
+    #                 to_score_embs.norm(p=2, dim=1, keepdim=True) / \
+    #                 embs_to_score_against.norm(p=2, dim=1, keepdim=True).t()
+    #     y_pred = score_matr.tolist()
+    #
+    #     return y_pred
+    #
+    # def score_candidates_lp(self, to_score_ids, to_score_embs, keys_to_score_against, embs_to_score_against, link_predictor, at=None, device="cpu"):
+    #
+    #     y_pred = []
+    #     for i in range(len(to_score_ids)):
+    #         input_embs = to_score_embs[i, :].repeat((embs_to_score_against.shape[0], 1))
+    #         # predictor_input = torch.cat([input_embs, all_emb], dim=1)
+    #         y_pred.append(link_predictor(input_embs, embs_to_score_against)[:, 1].tolist())  # 0 - negative, 1 - positive
+    #
+    #     return y_pred
 
-        ids = to_score_ids.tolist()
-        candidates = [set(list(self.element_lookup[id])) for id in ids]
-        all_keys = [key for key in self.name2repr]
-        # emb_matr = np.array([self.name2repr[key] for key in all_keys], dtype=np.int32)
-        # all_emb = self(torch.LongTensor(emb_matr)).detach().numpy()
-        # all_emb = normalize(all_emb, axis=1)
-        # ball_tree = BallTree(all_emb)
+    # def get_embeddings_for_scoring(self, device, **kwargs):
+    #     emb_matr = np.array([self.name2repr[key] for key in self.get_keys_for_scoring()], dtype=np.int32)
+    #     embs_to_score_against = self(torch.LongTensor(self.scorer_all_emb).to(device))
+    #     return embs_to_score_against
 
-        y_true = [[1. if all_keys[i] in cand else 0. for i in range(len(all_keys))] for cand in candidates]
+    # def get_keys_for_scoring(self):
+    #     return self.scorer_all_keys
 
-        y_pred = []
-        to_score_embs = to_score_embs.detach().numpy()
-        to_score_embs = normalize(to_score_embs, axis=1)
-        for ind in range(to_score_embs.shape[0]):
-            curr_to_score = to_score_embs[ind].reshape(1, -1)
-            _, closest = self.ball_tree.query(curr_to_score, k=at if type(at) is int else max(at))
-            scores = (curr_to_score @ self.all_emb[closest.ravel(), :].T).ravel()
-            closest = closest.ravel().tolist()
-            y_pred.append([])
-            for i in range(len(all_keys)):
-                if i in closest:
-                    y_pred[-1].append(scores[closest.index(i)])
-                else:
-                    y_pred[-1].append(-1.)
-
-        # score_matr = (to_score_embs @ all_emb.t()) / \
-        #             to_score_embs.norm(p=2, dim=1, keepdim=True) / \
-        #             all_emb.norm(p=2, dim=1, keepdim=True).t()
-        # y_pred = score_matr.tolist()
-
-        if isinstance(at, Iterable):
-            scores = {f"ndcg@{k}": ndcg_score(y_true, y_pred, k=k) for k in at}
-        else:
-            scores = {f"ndcg@{at}": ndcg_score(y_true, y_pred, k=at)}
-        return scores
-
-    def score_candidates_lp(self, to_score_ids, to_score_embs, link_predictor, at=None, device="cpu"):
-        if at is None:
-            at = [1, 3, 5, 10]
-
-        ids = to_score_ids.tolist()
-        candidates = [set(list(self.element_lookup[id])) for id in ids]
-        all_keys = [key for key in self.name2repr]
-        emb_matr = np.array([self.name2repr[key] for key in all_keys], dtype=np.int32)
-        all_emb = self(torch.LongTensor(emb_matr).to(device))
-
-        y_true = [[1. if all_keys[i] in cand else 0. for i in range(len(all_keys))] for cand in candidates]
-
-        y_pred = []
-        for i in range(len(to_score_ids)):
-            input_embs = to_score_embs[i, :].repeat((all_emb.shape[0], 1))
-            # predictor_input = torch.cat([input_embs, all_emb], dim=1)
-            y_pred.append(link_predictor(input_embs, all_emb)[:, 1].tolist())  # 0 - negative, 1 - positive
-
-        if isinstance(at, Iterable):
-            scores = {f"ndcg@{k}": ndcg_score(y_true, y_pred, k=k) for k in at}
-        else:
-            scores = {f"ndcg@{at}": ndcg_score(y_true, y_pred, k=at)}
-        return scores
-
-    def score_candidates(self, to_score_ids, to_score_embs, link_predictor=None, at=None, type=None, device="cpu"):
-        if type == "nn":
-            scores = self.score_candidates_lp(to_score_ids, to_score_embs, link_predictor, at=at, device=device)
-        elif type == "inner_prod":
-            scores = self.score_candidates_cosine(to_score_ids, to_score_embs, at=at)
-        else:
-            raise ValueError(f"`type` can be either `nn` or `inner_prod` but `{type}` given")
-        return scores
-
-
-
+    # def score_candidates(self, to_score_ids, to_score_embs, link_predictor=None, at=None, type=None, device="cpu"):
+    #
+    #     if at is None:
+    #         at = [1, 3, 5, 10]
+    #
+    #     to_score_ids = to_score_ids.tolist()
+    #
+    #     candidates = self.get_gt_candidates(to_score_ids)
+    #     keys_to_score_against = self.get_keys_for_scoring()
+    #
+    #     y_true = [[1. if key in cand else 0. for key in keys_to_score_against] for cand in candidates]
+    #
+    #     embs_to_score_against = self.get_embeddings_for_scoring(device=to_score_embs.device)
+    #     # emb_matr = np.array([self.name2repr[key] for key in keys_to_score_against], dtype=np.int32)
+    #     # embs_to_score_against = self(torch.LongTensor(emb_matr).to(to_score_embs.device))
+    #
+    #     if type == "nn":
+    #         y_pred = self.score_candidates_lp(to_score_ids, to_score_embs, keys_to_score_against, embs_to_score_against, link_predictor, at=at, device=device)
+    #     elif type == "inner_prod":
+    #         y_pred = self.score_candidates_cosine(to_score_ids, to_score_embs, keys_to_score_against, embs_to_score_against, at=at)
+    #     else:
+    #         raise ValueError(f"`type` can be either `nn` or `inner_prod` but `{type}` given")
+    #
+    #     if isinstance(at, Iterable):
+    #         scores = {f"ndcg@{k}": ndcg_score(y_true, y_pred, k=k) for k in at}
+    #     else:
+    #         scores = {f"ndcg@{at}": ndcg_score(y_true, y_pred, k=at)}
+    #     return scores
 
 
 class ElementEmbedderWithBpeSubwords(ElementEmbedderWithCharNGramSubwords, nn.Module):
     def __init__(self, elements, nodes, emb_size, tokenizer_path, num_buckets=100000, max_len=10):
-        ElementEmbedderBase.__init__(self, elements=elements, nodes=nodes, compact_dst=False)
-        nn.Module.__init__(self)
+        self.tokenizer_path = tokenizer_path
+        ElementEmbedderWithCharNGramSubwords.__init__(
+            self, elements=elements, nodes=nodes, emb_size=emb_size, num_buckets=num_buckets,
+            max_len=max_len
+        )
 
-        self.emb_size = emb_size
-
+    def init_subwords(self, elements, num_buckets, max_len):
         from SourceCodeTools.nlp.embed.bpe import load_bpe_model, make_tokenizer
-        tokenize = make_tokenizer(load_bpe_model(tokenizer_path))
+        tokenize = make_tokenizer(load_bpe_model(self.tokenizer_path))
 
         names = elements['dst']
         reprs = names.map(tokenize) \
@@ -187,7 +217,7 @@ class ElementEmbedderWithBpeSubwords(ElementEmbedderWithCharNGramSubwords, nn.Mo
 
         self.name2repr = dict(zip(names, reprs))
 
-        self.embed = nn.Embedding(num_buckets, emb_size, padding_idx=0)
+        self.embed = nn.Embedding(num_buckets, self.emb_size, padding_idx=0)
 
 
 class NameEmbedderWithGroups(ElementEmbedderWithBpeSubwords):
@@ -195,11 +225,21 @@ class NameEmbedderWithGroups(ElementEmbedderWithBpeSubwords):
         super(NameEmbedderWithGroups, self).__init__(elements, nodes, emb_size, tokenizer_path, num_buckets, max_len)
 
         self.group_lookup = {}
-        for id_, group_ in self.elements[["dst", "group"]].values:
-            if id_ not in self.element_lookup:
+        for dst, group_ in self.elements[["dst", "group"]].values:
+            if dst not in self.element_lookup:
                 self.group_lookup[group_] = []
 
-            self.group_lookup[group_].append(id_)
+            self.group_lookup[group_].append(dst)
+
+        self.id2group = dict(zip(self.elements["id"], self.elements["group"]))
+
+    def get_cand_to_score_against(self, ids):
+        # TODO this way the score depends on the batch size which is bad
+        all_keys = []
+        [all_keys.extend(self.group_lookup[self.id2group[id]]) for id in ids]
+        all_keys = list(set(all_keys))
+        return all_keys
+
 
 
 
---
+++ SourceCodeTools/models/graph/train/Scorer.py
@@ -0,0 +1,132 @@
+import random
+from collections import Iterable
+
+import torch
+import numpy as np
+from sklearn.metrics import ndcg_score
+from sklearn.neighbors._ball_tree import BallTree
+from sklearn.preprocessing import normalize
+
+
+class Scorer:
+    def __init__(self, num_embs, emb_size, src2dst, neighbours_to_sample=5):
+        self.scorer_num_emb = num_embs
+        self.scorer_emb_size = emb_size
+        self.scorer_src2dst = src2dst
+
+        self.scorer_all_emb = np.zeros((num_embs, emb_size))
+        self.scorer_all_keys = self.get_cand_to_score_against(None)
+        self.scorer_key_order = dict(zip(self.scorer_all_keys, range(len(self.scorer_all_keys))))
+        self.scorer_ball_tree = None
+        self.neighbours_to_sample = min(neighbours_to_sample, self.scorer_num_emb)
+
+    def prepare_index(self):
+        self.scorer_ball_tree = BallTree(normalize(self.scorer_all_emb, axis=1))
+
+    def sample_closest_negative(self, ids, k=None):
+        if k is None:
+            k = self.neighbours_to_sample
+
+        assert ids is not None
+        seed_pool = []
+        [seed_pool.append(self.scorer_src2dst[id]) for id in ids]
+        nested_negative = self.get_closest_to_keys(seed_pool, k=k+1)
+
+        negative = []
+        for neg in nested_negative:
+            negative.extend(random.choices(neg, k=k))
+        return negative
+
+    def get_closest_to_keys(self, key_groups, k=None):
+        possible_targets = []
+        for key_group in key_groups:
+            closest_keys = []
+            for key in key_group:
+                _, closest = self.scorer_ball_tree.query(
+                    self.scorer_all_emb[self.scorer_key_order[key]].reshape(1, -1), k=k
+                )
+                closest_keys.extend(self.scorer_all_keys[c] for c in closest.ravel())
+            closest_keys_ = list(set(closest_keys) - set(key_group))
+            if len(closest_keys_) == 0:
+                # backup strategy
+                closest_keys_ = random.choices(list(set(self.scorer_all_keys) - set(key_group)), k=k-1)
+            possible_targets.append(closest_keys_)
+            # possible_targets.extend(self.scorer_all_keys[c] for c in closest.ravel())
+        return possible_targets
+        # return list(set(possible_targets) - set(keys))
+
+    def set_embed(self, ids, embs):
+
+        ids = np.array(list(map(self.scorer_key_order.get, ids.tolist())))
+        self.scorer_all_emb[ids, :] = embs
+
+        # for ind, id in enumerate(ids):
+        #     self.all_embs[self.key_order[id], :] = embs[ind, :]
+
+    def score_candidates_cosine(self, to_score_ids, to_score_embs, keys_to_score_against, embs_to_score_against, at=None):
+
+        score_matr = (to_score_embs @ embs_to_score_against.t()) / \
+                    to_score_embs.norm(p=2, dim=1, keepdim=True) / \
+                    embs_to_score_against.norm(p=2, dim=1, keepdim=True).t()
+        y_pred = score_matr.tolist()
+
+        return y_pred
+
+    def score_candidates_lp(self, to_score_ids, to_score_embs, keys_to_score_against, embs_to_score_against, link_predictor, at=None):
+
+        y_pred = []
+        for i in range(len(to_score_ids)):
+            input_embs = to_score_embs[i, :].repeat((embs_to_score_against.shape[0], 1))
+            # predictor_input = torch.cat([input_embs, all_emb], dim=1)
+            y_pred.append(link_predictor(input_embs, embs_to_score_against)[:, 1].tolist())  # 0 - negative, 1 - positive
+
+        return y_pred
+
+    def get_gt_candidates(self, ids):
+        candidates = [set(list(self.scorer_src2dst[id])) for id in ids]
+        return candidates
+
+    def get_cand_to_score_against(self, ids):
+        all_keys = set()
+
+        [all_keys.update(self.scorer_src2dst[key]) for key in self.scorer_src2dst]
+        return sorted(list(all_keys))  # list(self.elem2id[a] for a in all_keys)
+
+    def get_embeddings_for_scoring(self, device, **kwargs):
+        return torch.Tensor(self.scorer_all_emb).to(device)
+
+    def get_keys_for_scoring(self):
+        return self.scorer_all_keys
+
+    def score_candidates(self, to_score_ids, to_score_embs, link_predictor=None, at=None, type=None, device="cpu"):
+
+        if at is None:
+            at = [1, 3, 5, 10]
+
+        to_score_ids = to_score_ids.tolist()
+
+        candidates = self.get_gt_candidates(to_score_ids)
+        # keys_to_score_against = self.get_cand_to_score_against(to_score_ids)
+        keys_to_score_against = self.get_keys_for_scoring()
+
+        y_true = [[1. if key in cand else 0. for key in keys_to_score_against] for cand in candidates]
+
+        embs_to_score_against = self.get_embeddings_for_scoring(device=to_score_embs.device)
+
+        if type == "nn":
+            y_pred = self.score_candidates_lp(
+                to_score_ids, to_score_embs, keys_to_score_against, embs_to_score_against,
+                link_predictor, at=at
+            )
+        elif type == "inner_prod":
+            y_pred = self.score_candidates_cosine(
+                to_score_ids, to_score_embs, keys_to_score_against, embs_to_score_against, at=at
+            )
+        else:
+            raise ValueError(f"`type` can be either `nn` or `inner_prod` but `{type}` given")
+
+        if isinstance(at, Iterable):
+            scores = {f"ndcg@{k}": ndcg_score(y_true, y_pred, k=k) for k in at}
+        else:
+            scores = {f"ndcg@{at}": ndcg_score(y_true, y_pred, k=at)}
+        return scores
