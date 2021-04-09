b72b18bdfd2e255db759d5b4aae478221f0a9ebb
Vitaly R <mortiv16@gmail.com>
2021-04-09 13:13:27 +0300

    fixed script behaviour

--- SourceCodeTools/code/data/sourcetrail/sourcetrail_draw_graph.py
+++ SourceCodeTools/code/data/sourcetrail/sourcetrail_draw_graph.py
@@ -9,6 +9,24 @@ def visualize(nodes, edges, output_path):
 
     edges = edges[edges["type"].apply(lambda x: not x.endswith("_rev"))]
 
+    from SourceCodeTools.code.data.sourcetrail.Dataset import get_global_edges, ensure_connectedness
+
+    # def remove_ast_edges(nodes, edges):
+    #     global_edges = get_global_edges()
+    #     global_edges.add("subword")
+    #     is_global = lambda type: type in global_edges
+    #     edges = edges.query("type.map(@is_global)", local_dict={"is_global": is_global})
+    #     return nodes, edges # ensure_connectedness(nodes, edges)
+
+    # def remove_global_edges(nodes, edges):
+    #     global_edges = get_global_edges()
+    #     global_edges.add("global_mention")
+    #     is_ast = lambda type: type not in global_edges
+    #     edges = edges.query("type.map(@is_ast)", local_dict={"is_ast": is_ast})
+    #     return nodes, edges # ensure_connectedness(nodes, edges)
+    #
+    # nodes, edges = remove_global_edges(nodes, edges)
+
     id2name = dict(zip(nodes['id'], nodes['serialized_name']))
     id2type = dict(zip(nodes['id'], nodes['type']))
 
@@ -32,3 +50,4 @@ def visualize(nodes, edges, output_path):
 if __name__ == "__main__":
     nodes = pd.read_pickle("common_nodes.bz2")
     edges = pd.read_pickle("common_edges.bz2")
+    visualize(nodes, edges, "test.pdf")
