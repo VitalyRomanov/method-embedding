d4abb59cd66ea65816070eb9817bfc3548e81d78
Vitaly R <mortiv16@gmail.com>
2021-06-23 15:48:05 +0300

    added description

--- SourceCodeTools/tabular/common.py
+++ SourceCodeTools/tabular/common.py
@@ -2,6 +2,13 @@ import numpy
 
 
 def compact_property(values, return_order=False, index_from_one=False):
+    """
+    Returns a map from the original value to the compact index
+    :param values:
+    :param return_order:
+    :param index_from_one:
+    :return:
+    """
     uniq = numpy.unique(values)
     if index_from_one:
         index = range(1, uniq.size + 1)
