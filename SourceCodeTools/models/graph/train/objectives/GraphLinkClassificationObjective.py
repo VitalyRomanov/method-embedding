ef038142ef77230d415fbeb90c960b88098af060
Vitaly R <mortiv16@gmail.com>
2021-10-11 21:44:23 +0300

    made sure transr objective works

--- SourceCodeTools/models/graph/train/objectives/GraphLinkClassificationObjective.py
+++ SourceCodeTools/models/graph/train/objectives/GraphLinkClassificationObjective.py
@@ -8,10 +8,9 @@ from SourceCodeTools.code.data.sourcetrail.SubwordMasker import SubwordMasker
 from SourceCodeTools.mltools.torch import _compute_accuracy
 from SourceCodeTools.models.graph.ElementEmbedder import GraphLinkSampler
 from SourceCodeTools.models.graph.ElementEmbedderBase import ElementEmbedderBase
-from SourceCodeTools.models.graph.LinkPredictor import LinkClassifier, BilinearLinkPedictor
+from SourceCodeTools.models.graph.LinkPredictor import BilinearLinkPedictor, TransRLinkPredictor
 from SourceCodeTools.models.graph.train.Scorer import Scorer
 from SourceCodeTools.models.graph.train.objectives.GraphLinkObjective import GraphLinkObjective
-# from SourceCodeTools.models.graph.train.sampling_multitask2 import _compute_accuracy
 from SourceCodeTools.tabular.common import compact_property
 
 
@@ -29,6 +28,7 @@ class GraphLinkClassificationObjective(GraphLinkObjective):
             masker=masker, measure_ndcg=measure_ndcg, dilate_ndcg=dilate_ndcg
         )
         self.measure_ndcg = False
+        self.update_embeddings_for_queries = False
 
     def create_graph_link_sampler(self, data_loading_func, nodes):
         self.target_embedder = TargetLinkMapper(
@@ -42,55 +42,7 @@ class GraphLinkClassificationObjective(GraphLinkObjective):
         self.label_dtype = torch.long
 
     def create_positive_labels(self, ids):
-        return self.target_embedder.get_labels(ids)
-
-    # def _logits_nodes(self, node_embeddings,
-    #                   elem_embedder, link_predictor, create_dataloader,
-    #                   src_seeds, negative_factor=1, train_embeddings=True):
-    #     k = negative_factor
-    #     indices = self.seeds_to_global(src_seeds).tolist()
-    #     batch_size = len(indices)
-    #
-    #     node_embeddings_batch = node_embeddings
-    #     dst_indices, labels_pos = elem_embedder[indices]  # this assumes indices is torch tensor
-    #
-    #     # dst targets are not unique
-    #     unique_dst, slice_map = self._handle_non_unique(dst_indices)
-    #     assert unique_dst[slice_map].tolist() == dst_indices.tolist()
-    #
-    #     dataloader = create_dataloader(unique_dst)
-    #     input_nodes, dst_seeds, blocks = next(iter(dataloader))
-    #     blocks = [blk.to(self.device) for blk in blocks]
-    #     assert dst_seeds.shape == unique_dst.shape
-    #     assert dst_seeds.tolist() == unique_dst.tolist()
-    #     unique_dst_embeddings = self._logits_batch(input_nodes, blocks, train_embeddings)  # use_types, ntypes)
-    #     dst_embeddings = unique_dst_embeddings[slice_map.to(self.device)]
-    #
-    #     # self.target_embedder.set_embed(unique_dst.detach().cpu().numpy(), unique_dst_embeddings.detach().cpu().numpy())
-    #
-    #     node_embeddings_neg_batch = node_embeddings_batch.repeat(k, 1)
-    #     # negative_indices = torch.tensor(elem_embedder.sample_negative(
-    #     #     batch_size * k), dtype=torch.long)  # embeddings are sampled from 3/4 unigram distribution
-    #     negative_indices = torch.tensor(elem_embedder.sample_negative(
-    #         batch_size * k, ids=indices), dtype=torch.long)  # closest negative
-    #     unique_negative, slice_map = self._handle_non_unique(negative_indices)
-    #     assert unique_negative[slice_map].tolist() == negative_indices.tolist()
-    #
-    #     dataloader = create_dataloader(unique_negative)
-    #     input_nodes, dst_seeds, blocks = next(iter(dataloader))
-    #     blocks = [blk.to(self.device) for blk in blocks]
-    #     assert dst_seeds.shape == unique_negative.shape
-    #     assert dst_seeds.tolist() == unique_negative.tolist()
-    #     unique_negative_random = self._logits_batch(input_nodes, blocks, train_embeddings)  # use_types, ntypes)
-    #     negative_random = unique_negative_random[slice_map.to(self.device)]
-    #     labels_neg = torch.full((batch_size * k,), self.negative_label, dtype=self.label_dtype)
-    #
-    #     # self.target_embedder.set_embed(unique_negative.detach().cpu().numpy(), unique_negative_random.detach().cpu().numpy())
-    #
-    #     nodes = torch.cat([node_embeddings_batch, node_embeddings_neg_batch], dim=0)
-    #     embs = torch.cat([dst_embeddings, negative_random], dim=0)
-    #     labels = torch.cat([labels_pos, labels_neg], 0).to(self.device)
-    #     return nodes, embs, labels
+        return torch.LongTensor(self.target_embedder.get_labels(ids))
 
     def compute_acc_loss(self, node_embs_, element_embs_, labels):
         logits = self.link_predictor(node_embs_, element_embs_)
@@ -103,6 +55,42 @@ class GraphLinkClassificationObjective(GraphLinkObjective):
 
         return acc, loss
 
+
+class TransRObjective(GraphLinkClassificationObjective):
+    def __init__(
+            self, graph_model, node_embedder, nodes, data_loading_func, device,
+            sampling_neighbourhood_size, batch_size,
+            tokenizer_path=None, target_emb_size=None, link_predictor_type="inner_prod", masker: SubwordMasker = None,
+            measure_ndcg=False, dilate_ndcg=1
+    ):
+        super().__init__(
+            "TransR", graph_model, node_embedder, nodes, data_loading_func, device,
+            sampling_neighbourhood_size, batch_size,
+            tokenizer_path=tokenizer_path, target_emb_size=target_emb_size, link_predictor_type=link_predictor_type,
+            masker=masker, measure_ndcg=measure_ndcg, dilate_ndcg=dilate_ndcg
+        )
+
+    def create_link_predictor(self):
+        self.link_predictor = TransRLinkPredictor(
+            input_dim=self.target_emb_size, rel_dim=30,
+            num_relations=self.target_embedder.num_classes
+        ).to(self.device)
+        # self.positive_label = 1
+        self.negative_label = -1
+        self.label_dtype = torch.long
+
+    def compute_acc_loss(self, node_embs_, element_embs_, labels):
+
+        num_examples = len(labels) // 2
+        anchor = node_embs_[:num_examples, :]
+        positive = element_embs_[:num_examples, :]
+        negative = element_embs_[num_examples:, :]
+        labels_ = labels[:num_examples]
+
+        loss, sim = self.link_predictor(anchor, positive, negative, labels_)
+        acc = _compute_accuracy(sim, labels >= 0)
+
+        return acc, loss
 
 class TargetLinkMapper(GraphLinkSampler):
     def __init__(self, elements, nodes):
@@ -130,7 +118,8 @@ class TargetLinkMapper(GraphLinkSampler):
 
     def __getitem__(self, ids):
         self.cached_ids = ids
-        node_ids, self.cached_labels = zip(*(rnd.choice(self.element_lookup[id]) for id in ids))
+        node_ids, labels = zip(*(rnd.choice(self.element_lookup[id]) for id in ids))
+        self.cached_labels = list(labels)
         return np.array(node_ids, dtype=np.int32)#, torch.LongTensor(np.array(labels, dtype=np.int32))
 
     def get_labels(self, ids):
@@ -138,7 +127,7 @@ class TargetLinkMapper(GraphLinkSampler):
             return self.cached_labels
         else:
             node_ids, labels = zip(*(rnd.choice(self.element_lookup[id]) for id in ids))
-            return labels
+            return list(labels)
 
     def sample_negative(self, size, ids=None, strategy="w2v"):  # TODO switch to w2v?
         if strategy == "w2v":
