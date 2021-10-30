d132ed5247fd0165464afb16f82537a57e063c45
Vitaly R <mortiv16@gmail.com>
2021-10-30 19:07:00 +0300

    make sure nothing fails when nn classifier used

--- SourceCodeTools/models/graph/ElementEmbedder.py
+++ SourceCodeTools/models/graph/ElementEmbedder.py
@@ -12,13 +12,13 @@ from SourceCodeTools.models.nlp.TorchEncoder import LSTMEncoder, Encoder
 
 
 class GraphLinkSampler(ElementEmbedderBase, Scorer):
-    def __init__(self, elements, nodes, compact_dst=True, dst_to_global=True, emb_size=None, device="cpu"):
+    def __init__(self, elements, nodes, compact_dst=True, dst_to_global=True, emb_size=None, device="cpu", method="inner_prod"):
         assert emb_size is not None
         ElementEmbedderBase.__init__(self, elements=elements, nodes=nodes, compact_dst=compact_dst, dst_to_global=dst_to_global)
-        Scorer.__init__(self, num_embs=len(self.elements["dst"].unique()), emb_size=emb_size, src2dst=self.element_lookup, device=device)
+        Scorer.__init__(self, num_embs=len(self.elements["dst"].unique()), emb_size=emb_size, src2dst=self.element_lookup, device=device, method=method)
 
     def sample_negative(self, size, ids=None, strategy="closest"):
-        if strategy == "w2v":
+        if strategy == "w2v" or self.scorer_index is None:
             negative = ElementEmbedderBase.sample_negative(self, size)
         else:
             negative = Scorer.sample_closest_negative(self, ids, k=size // len(ids))
