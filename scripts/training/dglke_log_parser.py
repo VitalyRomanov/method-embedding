ca7f19302966cd1878b34d521c7ca242d523ec0a
Vitaly R <mortiv16@gmail.com>
2022-02-16 17:55:59 +0300

    improve log parser

--- scripts/training/dglke_log_parser.py
+++ scripts/training/dglke_log_parser.py
@@ -13,6 +13,7 @@ def main():
         buffer_pos = [[]]
         buffer_neg = [[]]
         model_names = []
+        scores = [{}]
         for line in logfile:
             if "pos_loss" in line:
                 buffer_pos[-1].append(float(line.split("pos_loss: ")[-1]))
@@ -21,9 +22,21 @@ def main():
                 buffer_neg[-1].append(float(line.split("neg_loss: ")[-1]))
             elif "Save model" in line:
                 model_names.append(line.split("/")[-2])
+                scores[-1]["Model"] = model_names[-1]
+            elif "Test average MRR" in line:
+                scores[-1]["MRR"] = line.split(":")[-1].strip()
+            elif "Test average MR" in line:
+                scores[-1]["MR"] = line.split(":")[-1].strip()
+            elif "Test average HITS@1:" in line:
+                scores[-1]["HITS@1"] = line.split(":")[-1].strip()
+            elif "Test average HITS@3:" in line:
+                scores[-1]["HITS@3"] = line.split(":")[-1].strip()
+            elif "Test average HITS@10:" in line:
+                scores[-1]["HITS@10"] = line.split(":")[-1].strip()
                 buffer_pos.append([])
                 buffer_neg.append([])
                 steps.append([])
+                scores.append({})
 
     for s, v in zip(steps, buffer_pos):
         plt.plot(np.log10(s), np.log10(v))
@@ -48,6 +61,10 @@ def main():
     plt.legend(model_names)
     plt.savefig("overall_loss.png")
     plt.close()
+
+    import pandas as pd
+    s = pd.DataFrame.from_records(scores)
+    print(s.to_string)
 
 
 
