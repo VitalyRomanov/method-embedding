f7963269e1e998c2b04d4037fe2a93d4ca7f94f7
Vitaly R <mortiv16@gmail.com>
2022-03-29 22:30:07 +0200

    added option for selective column indexing

--- SourceCodeTools/code/data/SQLiteStorage.py
+++ SourceCodeTools/code/data/SQLiteStorage.py
@@ -3,7 +3,7 @@ import sqlite3
 import pandas as pd
 
 
-class SQLTable:
+class SQLiteStorage:
     def __init__(self, filename):
         self.conn = sqlite3.connect(filename)
         self.path = filename
@@ -12,17 +12,18 @@ class SQLTable:
         table.to_sql(table_name, con=self.conn, if_exists='replace', index=False, method="multi", chunksize=1000, **kwargs)
         self.create_index_for_table(table, table_name)
 
-    def add_records(self, table, table_name, **kwargs):
+    def add_records(self, table, table_name, create_index=None, **kwargs):
         table.to_sql(table_name, con=self.conn, if_exists='append', index=False, method="multi", chunksize=1000, **kwargs)
-        self.create_index_for_table(table, table_name)
+        if create_index is not None and create_index is not False:
+            self.create_index_for_table(table, table_name, create_index=create_index)
 
-    def create_index_for_table(self, table, table_name):
-        self.execute(
-            f"""
-            CREATE INDEX IF NOT EXISTS idx_{table_name} 
-            ON {table_name}({','.join(repr(col) for col in table.columns)})
-            """
-        )
+    def create_index_for_table(self, table, table_name, create_index=True):
+        if create_index is True:
+            index_columns = (repr(col) for col in table.columns)
+        else:
+            index_columns = create_index
+
+        self.create_index_for_columns(index_columns, table_name)
 
     def create_index_for_columns(self, columns, table_name):
         self.execute(
