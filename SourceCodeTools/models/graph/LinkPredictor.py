ee2d1b27102472d6f66bad73eeab7ed5be1f3d70
VitalyRomanov <mortiv16@gmail.com>
2022-07-07 20:02:42 +0300

    working on link prediction api

--- SourceCodeTools/models/graph/LinkPredictor.py
+++ SourceCodeTools/models/graph/LinkPredictor.py
@@ -47,22 +47,43 @@ class LinkClassifier(nn.Module):
 class BilinearLinkPedictor(nn.Module):
     def __init__(self, embedding_dim_1, embedding_dim_2, target_classes=2):
         super(BilinearLinkPedictor, self).__init__()
+        self.num_target_classes = target_classes
 
-        self.l1 = nn.Linear(embedding_dim_1, 300)
-        self.l2 = nn.Linear(embedding_dim_2, 300)
+        # self.src_l1 = nn.Linear(embedding_dim_1, embedding_dim_1)
+        # self.src_l2 = nn.Linear(embedding_dim_1, embedding_dim_1)
+        # self.src_l3 = nn.Linear(embedding_dim_1, embedding_dim_1)
+        #
+        # self.dst_l1 = nn.Linear(embedding_dim_2, embedding_dim_2)
+        # self.dst_l2 = nn.Linear(embedding_dim_2, embedding_dim_2)
+        # self.dst_l3 = nn.Linear(embedding_dim_2, embedding_dim_2)
+
+        self.l1 = nn.Linear(embedding_dim_1, 300, bias=False)
+        self.l2 = nn.Linear(embedding_dim_2, 300, bias=False)
         self.act = nn.Sigmoid()
         self.bilinear = nn.Bilinear(300, 300, target_classes)
 
     def forward(self, x1, x2):
-        return self.bilinear(self.act(self.l1(x1)), self.act(self.l2(x2)))
+
+        # x1 = self.act(self.src_l1(x1))
+        # x1 = self.act(self.src_l2(x1))
+        # x1 = self.act(self.src_l3(x1))
+        #
+        # x2 = self.act(self.dst_l1(x2))
+        # x2 = self.act(self.dst_l2(x2))
+        # x2 = self.act(self.dst_l3(x2))
+
+        if len(x1.shape) == 3 and x1.size(1) != x2.size(1):
+            x1 = torch.tile(x1, (1, x2.size(1), 1))
+
+        return self.bilinear(self.act(self.l1(x1)), self.act(self.l2(x2))).reshape(-1, self.num_target_classes)
 
 
-class CosineLinkPredictor(nn.Module):
+class CosineUndirectedLinkPredictor(nn.Module):
     def __init__(self, margin=0.):
         """
         Dummy link predictor, using to keep API the same
         """
-        super(CosineLinkPredictor, self).__init__()
+        super(CosineUndirectedLinkPredictor, self).__init__()
 
         self.cos = nn.CosineSimilarity(dim=-1)
         self.max_margin = torch.Tensor([margin])[0]
@@ -75,9 +96,9 @@ class CosineLinkPredictor(nn.Module):
         return torch.cat([1 - logit, logit], dim=-1)
 
 
-class L2LinkPredictor(nn.Module):
+class L2UndirectedLinkPredictor(nn.Module):
     def __init__(self, margin=1.):
-        super(L2LinkPredictor, self).__init__()
+        super(L2UndirectedLinkPredictor, self).__init__()
         self.margin = torch.Tensor([margin])[0]
 
     def forward(self, x1, x2):
@@ -86,6 +107,29 @@ class L2LinkPredictor(nn.Module):
         # this will not train
         logit = (torch.norm(x1 - x2, dim=-1, keepdim=True) < self.margin).float()
         return torch.cat([1 - logit, logit], dim=-1)
+
+
+class TranslationLinkPredictor(nn.Module):
+    def __init__(self, input_dim, rel_dim, margin=0.3):
+        super(TranslationLinkPredictor, self).__init__()
+        self.rel_dim = rel_dim
+        self.input_dim = input_dim
+        self.margin = margin
+
+        self.rel_emb = nn.Parameter(torch.randn(1, rel_dim) / rel_dim)
+        self.src_proj = nn.Linear(input_dim, rel_dim, bias=False)
+        self.dst_proj = nn.Linear(input_dim, rel_dim, bias=False)
+        self.triplet_loss = nn.TripletMarginLoss(margin=margin)
+
+    def forward(self, a, p, n, labels):
+
+        transl = self.src_proj(a) + self.rel_emb
+        m_p = self.dst_proj(p)
+        m_n = self.dst_proj(n)
+
+        sim = torch.norm(torch.cat([transl - m_p, transl - m_n], dim=0), dim=-1)
+
+        return self.triplet_loss(transl, m_p, m_n), sim < self.margin
 
 
 class TransRLinkPredictor(nn.Module):
