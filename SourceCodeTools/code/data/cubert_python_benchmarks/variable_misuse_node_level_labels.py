6bc41da50d92ad0b2fa3ecd4713e1c26e9abd8c5
Vitaly R <mortiv16@gmail.com>
2022-07-11 18:54:56 +0300

    added partition to var misuse node level labels dataset, useful for newer version of code

--- SourceCodeTools/code/data/cubert_python_benchmarks/variable_misuse_node_level_labels.py
+++ SourceCodeTools/code/data/cubert_python_benchmarks/variable_misuse_node_level_labels.py
@@ -5,6 +5,7 @@ from tqdm import tqdm
 
 from SourceCodeTools.code.common import read_edges
 from SourceCodeTools.code.data.file_utils import unpersist, persist
+from SourceCodeTools.code.data.cubert_python_benchmarks.partitioning import add_splits
 
 
 def get_node_labels(dataset_path):
@@ -40,7 +41,7 @@ def get_node_labels(dataset_path):
                 nodes_in_file.add(source_node_id)
                 nodes_in_file.add(target_node_id)
 
-                if file_id_to_process != None:
+                if file_id_to_process is not None:
                     comment = id2comment[file_id_to_process]
 
                     if comment is not None:
@@ -70,45 +71,13 @@ def get_node_labels(dataset_path):
                 nodes_in_file.add(source_node_id)
                 nodes_in_file.add(target_node_id)
 
-
-
-    # for file_id, file_edges in tqdm(edges.groupby("file_id")):
-    #     comment = id2comment[file_id]
-    #
-    #     if comment is None:
-    #         continue
-    #
-    #     if comment.startswith("original"):
-    #         continue
-    #
-    #     misused_vars = set(comment.split(" ")[-1].strip("`").split("`->`"))
-    #
-    #     if len(misused_vars) != 2:
-    #         print(f"Error in {file_id}")
-    #         continue
-    #
-    #     all_nodes = set(file_edges["source_node_id"].append(file_edges["target_node_id"]))
-    #
-    #     for node_id in all_nodes:
-    #         if nodeid2type[node_id] == "mention":
-    #             node_name = nodeid2name[node_id].split("@")[0]
-    #             if node_name in misused_vars:
-    #                 misuse_labels.append({
-    #                     "src": node_id,
-    #                     "dst": "misused"
-    #                 })
-    #             else:
-    #                 misuse_labels.append({
-    #                     "src": node_id,
-    #                     "dst": "correct"
-    #                 })
-
     labels = pd.DataFrame.from_records(misuse_labels)
+    partition = add_splits(labels[["src"]].rename({"src": "id"}, axis=1), 0.8)
+    labels_ = labels.merge(partition, how="left", left_on="src", right_on="id")
+    labels_.drop("id", axis=1, inplace=True)
+    assert (labels_.isna().sum() == 0).all()
 
-    persist(labels, join(dataset_path, "misuse_labels.json"))
-
-
-
+    persist(labels_, join(dataset_path, "misuse_labels.json"))
 
 
 if __name__ == "__main__":
