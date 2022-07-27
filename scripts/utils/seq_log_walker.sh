8df24089595463cedd7e371e69697ebf445a2a55
VitalyRomanov <mortiv16@gmail.com>
2022-07-27 12:26:17 +0300

    added seq walker

---
+++ scripts/utils/seq_log_walker.sh
@@ -0,0 +1,38 @@
+EVENTS_DIR=$1
+
+python seq_log_walker.py $EVENTS_DIR/RGGAN-2021-11-02-13-54-17-468378_name_pred_500_1_epoch/type_annotation/00_all_loc
+python seq_log_walker.py $EVENTS_DIR/RGGAN-2021-11-02-13-54-17-468378_name_pred_500_1_epoch/type_annotation/01_pop_loc
+python seq_log_walker.py $EVENTS_DIR/RGGAN-2021-11-02-13-54-17-468378_name_pred_500_1_epoch/type_annotation/02_pop_no_loc
+python seq_log_walker.py $EVENTS_DIR/RGGAN-2021-11-02-13-54-17-468378_name_pred_500_1_epoch/type_annotation/03_all_no_loc
+python seq_log_walker.py $EVENTS_DIR/RGGAN-2021-11-02-13-54-17-468378_name_pred_500_1_epoch/type_annotation/04_all_loc_cu
+python seq_log_walker.py $EVENTS_DIR/RGGAN-2021-11-02-13-54-17-468378_name_pred_500_1_epoch/type_annotation/05_pop_loc_cu
+python seq_log_walker.py $EVENTS_DIR/RGGAN-2021-11-02-13-54-17-468378_name_pred_500_1_epoch/type_annotation/06_pop_no_loc_cu
+python seq_log_walker.py $EVENTS_DIR/RGGAN-2021-11-02-13-54-17-468378_name_pred_500_1_epoch/type_annotation/07_all_no_loc_cu
+echo "\n\n"
+python seq_log_walker.py $EVENTS_DIR/RGGAN-2021-11-01-22-10-11-956335_edgepred_500/type_annotation/00_all_loc
+python seq_log_walker.py $EVENTS_DIR/RGGAN-2021-11-01-22-10-11-956335_edgepred_500/type_annotation/01_pop_loc
+python seq_log_walker.py $EVENTS_DIR/RGGAN-2021-11-01-22-10-11-956335_edgepred_500/type_annotation/02_pop_no_loc
+python seq_log_walker.py $EVENTS_DIR/RGGAN-2021-11-01-22-10-11-956335_edgepred_500/type_annotation/03_all_no_loc
+python seq_log_walker.py $EVENTS_DIR/RGGAN-2021-11-01-22-10-11-956335_edgepred_500/type_annotation/04_all_loc_cu
+python seq_log_walker.py $EVENTS_DIR/RGGAN-2021-11-01-22-10-11-956335_edgepred_500/type_annotation/05_pop_loc_cu
+python seq_log_walker.py $EVENTS_DIR/RGGAN-2021-11-01-22-10-11-956335_edgepred_500/type_annotation/06_pop_no_loc_cu
+python seq_log_walker.py $EVENTS_DIR/RGGAN-2021-11-01-22-10-11-956335_edgepred_500/type_annotation/07_all_no_loc_cu
+echo "\n\n"
+python seq_log_walker.py $EVENTS_DIR/RGGAN-2021-11-08-12-21-16-602379_transr_500_1_epoch/type_annotation/00_all_loc
+python seq_log_walker.py $EVENTS_DIR/RGGAN-2021-11-08-12-21-16-602379_transr_500_1_epoch/type_annotation/01_pop_loc
+python seq_log_walker.py $EVENTS_DIR/RGGAN-2021-11-08-12-21-16-602379_transr_500_1_epoch/type_annotation/02_pop_no_loc
+python seq_log_walker.py $EVENTS_DIR/RGGAN-2021-11-08-12-21-16-602379_transr_500_1_epoch/type_annotation/03_all_no_loc
+python seq_log_walker.py $EVENTS_DIR/RGGAN-2021-11-08-12-21-16-602379_transr_500_1_epoch/type_annotation/04_all_loc_cu
+python seq_log_walker.py $EVENTS_DIR/RGGAN-2021-11-08-12-21-16-602379_transr_500_1_epoch/type_annotation/05_pop_loc_cu
+python seq_log_walker.py $EVENTS_DIR/RGGAN-2021-11-08-12-21-16-602379_transr_500_1_epoch/type_annotation/06_pop_no_loc_cu
+python seq_log_walker.py $EVENTS_DIR/RGGAN-2021-11-08-12-21-16-602379_transr_500_1_epoch/type_annotation/07_all_no_loc_cu
+echo "\n\n"
+python seq_log_walker.py $EVENTS_DIR/RGGAN-2021-11-01-20-55-31-532378_node_clf_500_1_epoch/type_annotation/00_all_loc
+python seq_log_walker.py $EVENTS_DIR/RGGAN-2021-11-01-20-55-31-532378_node_clf_500_1_epoch/type_annotation/01_pop_loc
+python seq_log_walker.py $EVENTS_DIR/RGGAN-2021-11-01-20-55-31-532378_node_clf_500_1_epoch/type_annotation/02_pop_no_loc
+python seq_log_walker.py $EVENTS_DIR/RGGAN-2021-11-01-20-55-31-532378_node_clf_500_1_epoch/type_annotation/03_all_no_loc
+python seq_log_walker.py $EVENTS_DIR/RGGAN-2021-11-01-20-55-31-532378_node_clf_500_1_epoch/type_annotation/04_all_loc_cu
+python seq_log_walker.py $EVENTS_DIR/RGGAN-2021-11-01-20-55-31-532378_node_clf_500_1_epoch/type_annotation/05_pop_loc_cu
+python seq_log_walker.py $EVENTS_DIR/RGGAN-2021-11-01-20-55-31-532378_node_clf_500_1_epoch/type_annotation/06_pop_no_loc_cu
+python seq_log_walker.py $EVENTS_DIR/RGGAN-2021-11-01-20-55-31-532378_node_clf_500_1_epoch/type_annotation/07_all_no_loc_cu
+echo "\n\n"
