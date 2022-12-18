d85c346d383f82bae9bbdee3fc68af485a8856c9
VitalyRomanov <mortiv16@gmail.com>
2022-12-18 23:26:04 +0900

    rename columns

--- SourceCodeTools/code/data/ast_graph/draw_graph.py
+++ SourceCodeTools/code/data/ast_graph/draw_graph.py
@@ -1,13 +1,12 @@
 import pandas as pd
 
 
-
 def visualize(nodes, edges, output_path):
     import pygraphviz as pgv
 
     edges = edges[edges["type"].apply(lambda x: not x.endswith("_rev"))]
 
-    id2name = dict(zip(nodes['id'], nodes['serialized_name']))
+    id2name = dict(zip(nodes['id'], nodes['name']))
 
     g = pgv.AGraph(strict=False, directed=True)
 
@@ -15,8 +14,8 @@ def visualize(nodes, edges, output_path):
     auxiliaty_edge_types = PythonNodeEdgeDefinitions.auxiliary_edges()
 
     for ind, edge in edges.iterrows():
-        src = edge['source_node_id']
-        dst = edge['target_node_id']
+        src = edge['src']
+        dst = edge['dst']
         src_name = id2name[src]
         dst_name = id2name[dst]
         g.add_node(src_name, color="black")
