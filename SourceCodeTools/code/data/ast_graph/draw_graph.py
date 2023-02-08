ac4b94a0e748eaed3b64f575ad68240a71ed3943
VitalyRomanov <mortiv16@gmail.com>
2023-02-08 14:26:16 +0900

    unify api for drawing graphs

--- SourceCodeTools/code/data/ast_graph/draw_graph.py
+++ SourceCodeTools/code/data/ast_graph/draw_graph.py
@@ -1,9 +1,16 @@
-import pandas as pd
+from SourceCodeTools.code.data.file_utils import unpersist
 
 
-
-def visualize(nodes, edges, output_path, show_reverse=False):
+try:
     import pygraphviz as pgv
+except ModuleNotFoundError:
+    raise Exception("Install 'pygraphviz'")
+
+
+def visualize(
+        nodes, edges, output_path, show_reverse=False,
+        special_node_types=None, special_edge_types=None
+):
 
     nodes = nodes.rename({"serialized_name": "name"}, axis=1)
     edges = edges.rename({"source_node_id": "src", "target_node_id": "dst"}, axis=1)
@@ -12,19 +19,26 @@ def visualize(nodes, edges, output_path, show_reverse=False):
         edges = edges[edges["type"].apply(lambda x: not x.endswith("_rev"))]
 
     id2name = dict(zip(nodes['id'], nodes['name']))
+    id2type = dict(zip(nodes['id'], nodes['type']))
 
     g = pgv.AGraph(strict=False, directed=True)
 
     from SourceCodeTools.code.ast.python_ast2 import PythonNodeEdgeDefinitions
     auxiliaty_edge_types = PythonNodeEdgeDefinitions.auxiliary_edges()
+    if special_edge_types is not None:
+        auxiliaty_edge_types.update(special_edge_types)
+
+    auxiliaty_node_types = set()
+    if special_node_types is not None:
+        auxiliaty_node_types.update(special_node_types)
 
     for ind, edge in edges.iterrows():
         src = edge['src']
         dst = edge['dst']
         src_name = id2name[src]
         dst_name = id2name[dst]
-        g.add_node(src_name, color="black")
-        g.add_node(dst_name, color="black")
+        g.add_node(src_name, color="blue" if id2type[src] in auxiliaty_node_types else "black")
+        g.add_node(dst_name, color="blue" if id2type[src] in auxiliaty_node_types else "black")
         g.add_edge(src_name, dst_name, color="blue" if edge['type'] in auxiliaty_edge_types else "black")
         g_edge = g.get_edge(src_name, dst_name)
         g_edge.attr['label'] = edge['type']
@@ -34,6 +48,14 @@ def visualize(nodes, edges, output_path, show_reverse=False):
 
 
 if __name__ == "__main__":
-    nodes = pd.read_pickle("common_nodes.bz2")
-    edges = pd.read_pickle("common_edges.bz2")
-    visualize(nodes, edges, "test.pdf")
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("nodes")
+    parser.add_argument("edges")
+    parser.add_argument("output")
+    args = parser.parse_args()
+
+    nodes = unpersist(args.nodes)
+    edges = unpersist(args.edges)
+    visualize(nodes, edges, args.output)
--- SourceCodeTools/code/data/sourcetrail/sourcetrail_draw_graph.py
+++ SourceCodeTools/code/data/sourcetrail/sourcetrail_draw_graph.py
@@ -1,54 +1,88 @@
-import pandas as pd
+from SourceCodeTools.code.data.ast_graph.draw_graph import visualize as visualize_base
+from SourceCodeTools.code.data.file_utils import unpersist
+from SourceCodeTools.code.data.sourcetrail.sourcetrail_types import node_types, special_mapping
 
 
-def visualize(nodes, edges, output_path):
-    from SourceCodeTools.code.data.sourcetrail.sourcetrail_types import node_types
-    import pygraphviz as pgv
+def get_global_nodes():
+    return set(node_types.values())
 
-    global_types = list(node_types.values())
 
-    edges = edges[edges["type"].apply(lambda x: not x.endswith("_rev"))]
+def get_global_edges():
+    """
+    :return: Set of global edges and their reverses
+    """
+    types = set()
 
-    # def remove_ast_edges(nodes, edges):
-    #     global_edges = get_global_edges()
-    #     global_edges.add("subword")
-    #     is_global = lambda type: type in global_edges
-    #     edges = edges.query("type.map(@is_global)", local_dict={"is_global": is_global})
-    #     return nodes, edges # ensure_connectedness(nodes, edges)
+    for key, value in special_mapping.items():
+        types.add(key)
+        types.add(value)
 
-    # def remove_global_edges(nodes, edges):
-    #     global_edges = get_global_edges()
-    #     global_edges.add("global_mention")
-    #     is_ast = lambda type: type not in global_edges
-    #     edges = edges.query("type.map(@is_ast)", local_dict={"is_ast": is_ast})
-    #     return nodes, edges # ensure_connectedness(nodes, edges)
-    #
-    # nodes, edges = remove_global_edges(nodes, edges)
+    for _, value in node_types.items():
+        types.add(value + "_name")
 
-    id2name = dict(zip(nodes['id'], nodes['serialized_name']))
-    id2type = dict(zip(nodes['id'], nodes['type']))
+    return types
 
-    g = pgv.AGraph(strict=False, directed=True)
 
-    from SourceCodeTools.code.ast.python_ast2 import PythonNodeEdgeDefinitions
-    auxiliaty_edge_types = PythonNodeEdgeDefinitions.auxiliary_edges()
+def ensure_connectedness(nodes, edges):
+    unique_nodes = set(edges['src']) | set(edges['dst'])
+    num_unique_existing_nodes = nodes["id"].nunique()
 
-    for ind, edge in edges.iterrows():
-        src = edge['source_node_id']
-        dst = edge['target_node_id']
-        src_name = id2name[src]
-        dst_name = id2name[dst]
-        g.add_node(src_name, color="blue" if id2type[src] in global_types else "black")
-        g.add_node(dst_name, color="blue" if id2type[dst] in global_types else "black")
-        g.add_edge(src_name, dst_name, color="blue" if edge['type'] in auxiliaty_edge_types else "black")
-        g_edge = g.get_edge(src_name, dst_name)
-        g_edge.attr['label'] = edge['type']
+    if len(unique_nodes) == num_unique_existing_nodes:
+        return
 
-    g.layout("dot")
-    g.draw(output_path)
+    nodes = nodes.query("id in @unique_nodes", local_dict={"unique_nodes": unique_nodes})
+    return nodes, edges
+
+
+def remove_ast_edges_(nodes, edges):
+    global_edges = get_global_edges()
+    global_edges.add("subword")
+    is_global = lambda type: type in global_edges
+    edges = edges.query("type.map(@is_global)", local_dict={"is_global": is_global})
+    return ensure_connectedness(nodes, edges)
+
+
+def remove_global_edges_(nodes, edges):
+    global_edges = get_global_edges()
+    global_edges.add("global_mention")
+    is_ast = lambda type: type not in global_edges
+    edges = edges.query("type.map(@is_ast)", local_dict={"is_ast": is_ast})
+    return ensure_connectedness(nodes, edges)
+
+
+def visualize(
+        nodes, edges, output_path, show_reverse=False,
+        remove_ast_edges=False, remove_global_edges=False
+):
+    global_node_types = get_global_nodes()
+    global_edge_types = get_global_edges()
+
+    if show_reverse is False:
+        edges = edges[edges["type"].apply(lambda x: not x.endswith("_rev"))]
+
+    assert not (remove_global_edges is True and remove_ast_edges is False), "Cannot remove all edges"
+
+    if remove_global_edges:
+        nodes, edges = remove_global_edges_(nodes, edges)
+
+    if remove_ast_edges:
+        nodes, edges = remove_ast_edges_(nodes, edges)
+
+    visualize_base(
+        nodes, edges, output_path, show_reverse=show_reverse,
+        special_node_types=global_node_types,
+        special_edge_types=global_edge_types
+    )
 
 
 if __name__ == "__main__":
-    nodes = pd.read_pickle("common_nodes.bz2")
-    edges = pd.read_pickle("common_edges.bz2")
-    visualize(nodes, edges, "test.pdf")
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("nodes")
+    parser.add_argument("edges")
+    parser.add_argument("output")
+    args = parser.parse_args()
+
+    nodes = unpersist(args.nodes)
+    edges = unpersist(args.edges)
+    visualize(nodes, edges, args.output)
