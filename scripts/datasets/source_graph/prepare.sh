b6621ab2212fc72c67713f30a29fef42cf6eab1b
VitalyRomanov <mortiv16@gmail.com>
2023-02-12 13:39:41 +0900

    fixed bug that caused crash

--- scripts/datasets/source_graph/prepare.sh
+++ scripts/datasets/source_graph/prepare.sh
@@ -3,7 +3,7 @@ BPE_TOKENIZER=$(realpath "$2")
 OUTPUT_PATH=$(realpath "$3")
 RUN_DIR=$(realpath "$(dirname "$0")")
 
-#python $RUN_DIR/../../../SourceCodeTools/code/data/sourcetrail/DatasetCreator2.py $ENVIRONMENTS_PATH $OUTPUT_PATH --do_extraction --track_offsets --bpe_tokenizer $BPE_TOKENIZER
+python $RUN_DIR/../../../SourceCodeTools/code/data/sourcetrail/DatasetCreator2.py $ENVIRONMENTS_PATH $OUTPUT_PATH --do_extraction --track_offsets --bpe_tokenizer $BPE_TOKENIZER
 python $RUN_DIR/../../../SourceCodeTools/code/data/type_annotation_dataset/create_type_annotation_dataset.py envs $ENVIRONMENTS_PATH $OUTPUT_PATH/with_ast/annotations_dataset_no_default.json --remove_default --require_labels
 python $RUN_DIR/../../../SourceCodeTools/code/data/type_annotation_dataset/map_args_to_mentions.py $OUTPUT_PATH/with_ast $OUTPUT_PATH/with_ast/annotations_dataset_no_default_args_mapped.json --dataset_file $OUTPUT_PATH/with_ast/annotations_dataset_no_default.json
 python $RUN_DIR/../../../SourceCodeTools/code/data/type_annotation_dataset/split_dataset.py --data_path $OUTPUT_PATH/with_ast/annotations_dataset_no_default_args_mapped.json --random_seed 42 --name_suffix no_default_args_mapped
