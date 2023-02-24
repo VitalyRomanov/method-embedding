e88cd43d7a1b1f1d340706ae36b5ab865b183159
VitalyRomanov <mortiv16@gmail.com>
2023-02-25 01:19:31 +0900

    adding graph config

--- scripts/training/type_prediction/hybrid_type_prediction.py
+++ scripts/training/type_prediction/hybrid_type_prediction.py
@@ -1,14 +1,13 @@
-from pathlib import Path
-
-from SourceCodeTools.cli_arguments import TypePredictorTrainerArgumentParser
+from SourceCodeTools.cli_arguments import CodeBertEmbeddingExtractorArgumentParser, \
+    default_graph_config
 from SourceCodeTools.nlp.entity.utils.data import read_json_data
 from SourceCodeTools.nlp.trainers.hybrid_entity_trainer import HybridModelTrainer
 
 
 def main():
-    args = TypePredictorTrainerArgumentParser().parse()
+    config = CodeBertEmbeddingExtractorArgumentParser().parse()
 
-    if args.restrict_allowed:
+    if config["DATASET"]["restrict_allowed"]:
         allowed = {
             'str', 'Optional', 'int', 'Any', 'Union', 'bool', 'Callable', 'Dict', 'bytes', 'float', 'Description',
             'List', 'Sequence', 'Namespace', 'T', 'Type', 'object', 'HTTPServerRequest', 'Future', "Matcher"
@@ -17,18 +16,19 @@ def main():
         allowed = None
 
     train_data, test_data = read_json_data(
-        args.data_path, normalize=True, allowed=allowed, include_replacements=True, include_only="entities",
-        min_entity_count=args.min_entity_count
+        config["DATASET"]["data_path"], normalize=True, allowed=allowed, include_replacements=True, include_only="entities",
+        min_entity_count=config["DATASET"]["min_entity_count"]
     )
 
-    trainer_params = args.__dict__
-    trainer_params["suffix_prefix_buckets"] = 1
+    config["TRAINING"]["suffix_prefix_buckets"] = 1
+
+    graph_config = default_graph_config()
+    graph_config["TOKENIZER"]["tokenizer_path"] = "/Users/LTV/dev/method-embeddings/examples/sentencepiece_bpe.model"
 
     trainer = HybridModelTrainer(
         train_data, test_data,
-        model_params={},
-        trainer_params=trainer_params,
-        graph_subword_tokenizer_path=Path(__file__).parent.parent.parent.parent.joinpath("examples", "sentencepiece_bpe.model")
+        config=config,
+        graph_config=graph_config,
     )
     trainer.train_model()
 
