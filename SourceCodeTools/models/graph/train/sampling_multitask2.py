afb90912fefc80f5589783cb13468cffe6d8fa75
VitalyRomanov <mortiv16@gmail.com>
2023-03-23 19:20:37 +0900

    bring chaanges back, always use another file for misuse node classification

--- SourceCodeTools/models/graph/train/sampling_multitask2.py
+++ SourceCodeTools/models/graph/train/sampling_multitask2.py
@@ -480,9 +480,9 @@ class SamplingMultitaskTrainer:
         from SourceCodeTools.models.graph.train.objectives.NodeClassificationObjective import ClassifierTargetMapper
 
         def load_labels():
-            filecontent_path = Path(dataset.data_path).joinpath("misuse_edge_labels.json.bz2")
-            filecontent = unpersist(filecontent_path)[["src", "label"]]
-            return filecontent.rename({"label": "dst"}, axis=1)
+            filecontent_path = Path(dataset.data_path).joinpath("misuse_labels.json.bz2")
+            filecontent = unpersist(filecontent_path)[["src", "dst"]]
+            return filecontent  #.rename({"label": "dst"}, axis=1)
 
         self.objectives.append(
             self._create_node_level_objective(
