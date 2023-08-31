ccd3cee5345d9750b6f80142fb6e78ba45668a1d
VitalyRomanov <mortiv16@gmail.com>
2023-08-31 18:52:13 +0200

    update demo graph representation server

--- scripts/utils/graph_representation_server.py
+++ scripts/utils/graph_representation_server.py
@@ -5,8 +5,7 @@ import socketserver
 import sys
 from pathlib import Path
 
-from SourceCodeTools.code.data.ast_graph import source_code_to_graph
-
+from SourceCodeTools.code.data.ast_graph.build_ast_graph import source_code_to_graph
 
 PORT = 8000
 if len(sys.argv) > 1:
@@ -15,7 +14,7 @@ if len(sys.argv) > 1:
 
 class Handler(http.server.SimpleHTTPRequestHandler):
     def __init__(self, *args, **kwargs):
-        server_path = str(Path(os.getcwd()).joinpath(Path(__file__)).parent.joinpath("web_server").absolute())
+        server_path = str(Path(os.getcwd()).joinpath(Path(__file__)).parent.parent.parent.joinpath("res", "web_server").absolute())
         super().__init__(*args, directory=server_path, **kwargs)
 
     def do_POST(self):
@@ -24,11 +23,13 @@ class Handler(http.server.SimpleHTTPRequestHandler):
             data = json.loads(data_string.decode("utf8"))
 
             map_variation = {
-                "ast_graph": "with_mention",
-                "ast_graph_with_instances": "new_with_mention",
-                "cf_graph": "new_cf"
+                "ast_graph": "v2.5",
+                "ast_graph_with_instances": "v3.5",
+                "cf_graph": "v3.5_control_flow"
             }
-            nodes, edges = source_code_to_graph(data["source_code"], map_variation[data["variation"]])
+            graph = source_code_to_graph(data["source_code"], map_variation[data["variation"]])
+            nodes = graph["nodes"]
+            edges = graph["edges"]
 
             nodes = nodes.rename({"serialized_name": "name"}, axis=1)[["id", "type", "name"]]
             edges = edges.rename({"source_node_id": "src", "target_node_id": "dst"}, axis=1)[["id", "type", "src", "dst"]]
@@ -43,13 +44,15 @@ class Handler(http.server.SimpleHTTPRequestHandler):
             self.end_headers()
 
             self.wfile.write(bytes(json_responce, "utf8"))
-        except:
+        except Exception as e:
             self.send_response(501)
             self.send_header('Content-type', 'application/json')
             self.end_headers()
 
 
+#TODO
+# requires documentation
 
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
+with socketserver.TCPServer(("0.0.0.0", PORT), Handler) as httpd:
     print("serving at port", PORT)
     httpd.serve_forever()
